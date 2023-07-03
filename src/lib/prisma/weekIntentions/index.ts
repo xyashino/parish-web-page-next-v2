237550cd79f3e435367f8b330b42f40a09267763
Szczepan M <szczypiorpan123@gmail.com>
2023-07-03 14:53:13 +0200

    Add week-intentions basic functionality like deletion, creation  and selection one or many with relations

---
+++ src/lib/prisma/weekIntentions/index.ts
@@ -0,0 +1,4 @@
+export * from "./create-week-intentions";
+export * from "./delete-week-intentions";
+export * from "./get-many-week-intentions";
+export * from "./get-week-intentions-with-relations";
