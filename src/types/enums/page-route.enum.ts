f42e5aaf6c4a7af487d106194a5fb501e917b6ce
Szczepan M <szczypiorpan123@gmail.com>
2023-07-03 20:15:50 +0200

    Remove redundant fileAdd PageRoute enum for managing URLs.

---
+++ src/types/enums/page-route.enum.ts
@@ -0,0 +1,19 @@
+export enum PageRoute {
+  // ADMIN
+  LOGIN = "/admin/login",
+  ADMIN_HOME = "/admin",
+  CREATE_PROFILE = "/admin/profile/create",
+  EDIT_PROFILE = "/admin/profile/edit",
+
+  CREATE_INTENTIONS = "/admin/intentions/create",
+  LIST_INTENTIONS = "/admin/intentions",
+
+  CREATE_ANNOUNCEMENTS = "/admin/announcements/create",
+  LIST_ANNOUNCEMENTS = "/admin/announcements",
+
+  // CLIENT
+  CLIENT_HOME = "/",
+  CONTACT = "/contact",
+  INTENTIONS = "/intentions",
+  ANNOUNCEMENTS = "/announcements",
+}
