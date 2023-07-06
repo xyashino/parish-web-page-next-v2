e4022b0dc7520517ab69c34881679c5b786ca598
Szczepan M <szczypiorpan123@gmail.com>
2023-07-06 13:42:55 +0200

    Add fetchData utility function

---
+++ src/lib/utils/fetchData.ts
@@ -0,0 +1,22 @@
+const defaultConfig = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+export const fetchData = async <T>(
+  url: string,
+  config?: RequestInit,
+  override = true
+): Promise<T> => {
+  const result = await fetch(
+    "http://localhost:3000" + url,
+    override ? { ...defaultConfig, ...config } : config
+  );
+  const parsedResult = await result.json();
+  if (result.status !== 200) {
+    throw new Error(parsedResult.message || "Something went wrong!");
+  }
+  return parsedResult;
+};
