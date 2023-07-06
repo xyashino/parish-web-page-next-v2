8a2032b70bd8c8d64bc609c8f5c063ad566e2525
Szczepan M <szczypiorpan123@gmail.com>
2023-07-06 13:50:45 +0200

    Add CustomFormControlItem component
    This commit introduces a new component - 'CustomFormControlItem' under the Form components. This was needed as it's a versatile form control item, useful for creating interactive and dynamic forms. It accepts children, description and label as props, hence they can be customized as per usage.

---
+++ src/components/Form/CustomFormControlItem.tsx
@@ -0,0 +1,30 @@
+import React, { PropsWithChildren } from "react";
+import {
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+interface Props extends PropsWithChildren {
+  label?: string;
+  description?: string;
+}
+
+const CustomFormControlItem = <T extends {}>({
+  children,
+  description,
+  label,
+}: Props) => {
+  return (
+    <FormItem>
+      {!!label && <FormLabel>{label}</FormLabel>}
+      <FormControl className="items-center">{children}</FormControl>
+      {!!description && <FormDescription>{description}</FormDescription>}
+      <FormMessage />
+    </FormItem>
+  );
+};
+
+export default CustomFormControlItem;
