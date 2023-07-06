2c0bf9f96634e9d9da5e174cbbca4e1347bb0c7a
Szczepan M <szczypiorpan123@gmail.com>
2023-07-06 13:53:50 +0200

    Add new admin intentions create page

---
+++ src/app/(admin)/admin/intentions/create/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AdminPageTitle from "@/components/AdminPageTitle";
+import ModifyIntentions from "../../../../../components/ModifyWeekIntentions";
+
+const IntentionsCreatePage = async () => {
+  return (
+    <div className="flex flex-col space-y-6 p-4 rounded-xl lg:w-11/12 mx-auto">
+      <AdminPageTitle title="Dodajesz Intencje Parafialne" />
+      <ModifyIntentions />
+    </div>
+  );
+};
+
+export default IntentionsCreatePage;
