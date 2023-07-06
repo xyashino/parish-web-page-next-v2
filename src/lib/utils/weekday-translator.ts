631eb16e4b5a9985193c827d2d0c88ed28a4f2d4
Szczepan M <szczypiorpan123@gmail.com>
2023-07-06 13:29:05 +0200

    Add weekday translation utility
    A new utility file, weekday-translator.ts, was created that maps Weekday enums from Prisma client to their corresponding Polish names. This utility will be helpful to provide localized names for weekdays in the application.

---
+++ src/lib/utils/weekday-translator.ts
@@ -0,0 +1,10 @@
+import { Weekday } from "@prisma/client";
+export const X: Map<Weekday, string> = new Map([
+  [Weekday.MONDAY, "poniedziałek"],
+  [Weekday.FRIDAY, "piątek"],
+  [Weekday.SATURDAY, "sobota"],
+  [Weekday.SUNDAY, "niedziela"],
+  [Weekday.THURSDAY, "czwartek"],
+  [Weekday.TUESDAY, "wtorek"],
+  [Weekday.WEDNESDAY, "środa"],
+]);
