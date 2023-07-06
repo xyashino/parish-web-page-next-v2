78e020296d52c1ab3f89e8cf675ccec66013cc35
Szczepan M <szczypiorpan123@gmail.com>
2023-07-06 13:49:30 +0200

    Add Footer component
    A new Footer component has been added to the src/components directory. This component, which can optionally show an Admin icon, includes a logo and author info.

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import AdminIcon from "@/components/icons/AdminIcon";
+import Link from "next/link";
+import Logo from "@/components/Logo";
+import AuthorInfo from "@/components/Footer/authorInfo";
+import AdminDashboardIcon from "@/components/Footer/adminDashboardIcon";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  className?: string;
+  showAdminIcon?: true;
+}
+
+const Footer = ({ showAdminIcon, className }: Props) => {
+  const footerClassNames = cn(
+    "bg-foreground w-full text-background",
+    className
+  );
+
+  return (
+    <footer className={footerClassNames}>
+      <div className="container px-3 py-4 mx-auto flex items-center sm:flex-row flex-col">
+        <Logo className="hover:bg-transparent text-background" />
+        <div className="flex items-center justify-between flex-1">
+          <AuthorInfo />
+          {showAdminIcon && <AdminDashboardIcon />}
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
