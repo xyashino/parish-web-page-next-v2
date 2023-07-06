055094bbc1783dd22e52692780492461e4bff332
Szczepan M <szczypiorpan123@gmail.com>
2023-07-06 13:49:06 +0200

    Add AuthorInfo component to Footer
    A new AuthorInfo component was created and added to the Footer directory. This component gives copyright notice and links to the author’s GitHub profile. This adds an acknowledgment to the project creator and also serves as a contact point for project correspondence.

---
+++ src/components/Footer/authorInfo.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AuthorInfo = () => {
+  return (
+    <p className="text-sm text-background sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+      © {new Date().getFullYear()} Stworzone przez -
+      <a
+        href="https://github.com/xyashino"
+        target="_blank"
+        rel="noreferrer"
+        className="font-bold hover:underline ml-1"
+      >
+        xyashino
+      </a>
+    </p>
+  );
+};
+
+export default AuthorInfo;
