a82364d4abfccde6403b1f1b7c48f28d76bca3fa
Szczepan M <szczypiorpan123@gmail.com>
2023-07-06 13:46:09 +0200

    Refactor layout and remove unnecessary styles
    This commit simplifies the layout by removing redundant CSS classes in the body tags in the layout.tsx file. Previously, the body had a number of flex properties and max/min width values that weren't needed and made the styling complex. The toaster position remains unchanged.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,15 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import React, { PropsWithChildren } from "react";
 import { Toaster } from "react-hot-toast";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-      <html lang="pl">
-      <body
-          className={`${inter.className} min-h-screen max-w-screen min-w-screen min-w-screen flex flex-col bg-background items-center`}
-      >
-      {children}
-      <Toaster position="bottom-right" />
+    <html lang="pl">
+      <body className={`${inter.className} bg-background`}>
+        {children}
+        <Toaster position="bottom-right" />
       </body>
-      </html>
+    </html>
   );
 }
