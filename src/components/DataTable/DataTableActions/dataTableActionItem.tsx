7283364e66a8e834be0ce6419e4c9a16c802812d
Szczepan M <szczypiorpan123@gmail.com>
2023-07-08 13:59:13 +0200

    Add onClick prop to DataTableActionItem
    Introduce an optional onClick prop to the DataTableActionItem component. This increases the component's flexibility, allowing it to respond to click events whenever required.

--- src/components/DataTable/DataTableActions/dataTableActionItem.tsx
+++ src/components/DataTable/DataTableActions/dataTableActionItem.tsx
@@ -6,15 +6,16 @@ interface Props {
   className?: string;
   text: string;
   icon: React.ReactNode;
+  onClick?: () => void;
 }
 
-const DataTableActionItem = ({ className, text, icon }: Props) => {
+const DataTableActionItem = ({ className, text, icon, onClick }: Props) => {
   const classes = cn(
     "flex items-center justify-between w-full mx-2 text-foreground",
     className
   );
   return (
-    <DropdownMenuItem className="cursor-pointer">
+    <DropdownMenuItem className="cursor-pointer" onClick={onClick}>
       <span className={classes}>
         <span>{text}</span>
         {icon}
