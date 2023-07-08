8f45f32b2eaffb7809e755edaee382f1922df393
Szczepan M <szczypiorpan123@gmail.com>
2023-07-08 13:39:27 +0200

    Add Album import in get-albums.ts

--- src/lib/prisma/album/get-albums.ts
+++ src/lib/prisma/album/get-albums.ts
@@ -1,10 +1,11 @@
 import client from "@/lib/prisma";
+import { Album } from "@prisma/client";
 
 export const getAlbums = async () => {
-    try {
-        return await client.album.findMany();
-    } catch (error) {
-        console.error(error);
-        throw new Error("Something went wrong");
-    }
+  try {
+    return await client.album.findMany();
+  } catch (error) {
+    console.error(error);
+    throw new Error("Something went wrong");
+  }
 };
