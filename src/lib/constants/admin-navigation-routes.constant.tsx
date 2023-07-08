c7b05fc55270281e4b0e3ba6706d4915edb465ad
Szczepan M <szczypiorpan123@gmail.com>
2023-07-08 13:55:12 +0200

    Update icons in admin navigation

--- src/lib/constants/admin-navigation-routes.constant.tsx
+++ src/lib/constants/admin-navigation-routes.constant.tsx
@@ -1,75 +1,77 @@
-import AddIcon from "@/components/icons/AddIcon";
-import ListIcon from "@/components/icons/ListIcon";
-import ProfileIcon from "@/components/icons/ProfileIcon";
-import AnnouncementIcon from "@/components/icons/AnnouncementIcon";
-import IntentionIcon from "@/components/icons/IntentionIcon";
-import EditIcon from "@/components/icons/EditIcon";
-import GalleryIcon from "@/components/icons/GalleryIcon";
-import CategoriesIcon from "@/components/icons/CategoriesIcon";
-import AlbumIcon from "@/components/icons/AlbumIcon";
 import { NavigationItemType } from "@/types/interfaces/navigation.interface";
+import {
+  ListBulletIcon,
+  ImageIcon,
+  ArchiveIcon,
+  PersonIcon,
+  DashboardIcon,
+  ChatBubbleIcon,
+  FileTextIcon,
+  PlusCircledIcon,
+  Pencil2Icon,
+} from "@radix-ui/react-icons";
 
 export const ADMIN_NAVIGATION_ROUTES: NavigationItemType[] = [
   {
     href: "/admin/intentions",
     text: "Intencje",
-    icon: <IntentionIcon />,
+    icon: <ChatBubbleIcon />,
     nestedRoutes: [
       {
         href: "",
         text: "Wszystkie",
-        icon: <ListIcon />,
+        icon: <ListBulletIcon />,
       },
       {
         href: "/create",
         text: "Utwórz",
-        icon: <AddIcon />,
+        icon: <PlusCircledIcon />,
       },
     ],
   },
   {
     href: "/admin/announcements",
     text: "Ogłoszenia",
-    icon: <AnnouncementIcon />,
+    icon: <FileTextIcon />,
     nestedRoutes: [
       {
         href: "",
         text: "Wszystkie",
-        icon: <ListIcon />,
+        icon: <ListBulletIcon />,
       },
       {
         href: "/create",
         text: "Utwórz",
-        icon: <AddIcon />,
+        icon: <PlusCircledIcon />,
       },
     ],
   },
   {
     href: "/admin/gallery",
     text: "Galeria",
-    icon: <GalleryIcon />,
+    icon: <ImageIcon />,
     nestedRoutes: [
       {
         href: "/categories",
         text: "Katagorie",
-        icon: <CategoriesIcon />,
+        icon: <DashboardIcon />,
       },
       {
         href: "/albums",
         text: "Albumy",
-        icon: <AlbumIcon />,
+        icon: <ArchiveIcon />,
       },
     ],
   },
   {
     href: "/admin/administrators",
     text: "Administratorzy",
-    icon: <ProfileIcon />,
+    icon: <PersonIcon />,
     nestedRoutes: [
       {
         href: "/manage",
         text: "Zarządzaj",
-        icon: <EditIcon />,
+        icon: <Pencil2Icon />,
       },
     ],
   },
