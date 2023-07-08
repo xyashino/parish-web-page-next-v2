d16b69bb62d6a669ddc4941d57fdfa30cd467950
Szczepan M <szczypiorpan123@gmail.com>
2023-07-08 13:58:25 +0200

    Update icon prop to be optional in MobileNavigationDropdown

--- src/components/navigation/MobileNavigationDropdown/toggleButton.tsx
+++ src/components/navigation/MobileNavigationDropdown/toggleButton.tsx
@@ -1,10 +1,10 @@
-import React, { SyntheticEvent } from "react";
+import React, { ReactNode, SyntheticEvent } from "react";
 import DropdownIcon from "@/components/icons/DropdownIcon";
 
 interface Props {
   isOpen: boolean;
   toggle: (e: SyntheticEvent) => void;
-  icon: React.ReactNode;
+  icon?: ReactNode;
   text: string;
 }
 
@@ -19,7 +19,7 @@ const ToggleButton = ({ toggle, isOpen, text, icon }: Props) => {
       className={`flex w-full font-mono duration-500 appearance-none items-center justify-between rounded p-4 font-bold transition-colors hover:text-background hover:bg-foreground  ${toggleBtn}`}
     >
       <div className="flex items-center space-x-4">
-        <span className="text-xl lg:text-2xl">{icon}</span>
+        {!!icon && <span className="text-xl lg:text-2xl">{icon}</span>}
         <h2 className="font-bold uppercase text-sm md:text-md">{text}</h2>
       </div>
       <DropdownIcon className={`${toggleIcon} transition-transform`} />
