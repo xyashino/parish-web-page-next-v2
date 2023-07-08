04da2182b7ecabb3c2a0ebd026b6dbec857bd725
Szczepan M <szczypiorpan123@gmail.com>
2023-07-08 13:59:40 +0200

    Update alertTrigger component styles

--- src/components/ConfirmAlert/alertTrigger.tsx
+++ src/components/ConfirmAlert/alertTrigger.tsx
@@ -9,7 +9,7 @@ export interface AlertTriggerProps {
 
 const AlertTrigger = ({ triggerValue, className }: AlertTriggerProps) => {
   const triggerClasses = cn(
-    "inline-flex capitalize items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-primary text-primary-foreground hover:bg-primary/90 h-10 py-2 px-4",
+    "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 w-1/4",
     className
   );
   return (
