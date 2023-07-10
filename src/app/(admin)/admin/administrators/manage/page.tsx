aedffddfed795a4dbeff285a0323e60350a2f25e
Szczepan M <szczypiorpan123@gmail.com>
2023-07-10 19:31:09 +0200

    Add administrator management page in admin module
    This commit introduces a new page for managing administrators within the admin module. The page includes the necessary React component and basic layout, but does not include any additional functionalities yet. The title for the page is "Manage Administrators".

---
+++ src/app/(admin)/admin/administrators/manage/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AdminPageTitle from "@/components/AdminPageTitle";
+const AdministratorsManagePage = async () => {
+  return (
+    <div className="flex flex-col space-y-6">
+      <AdminPageTitle title="Zarządzaj Administatorami" />
+    </div>
+  );
+};
+
+export default AdministratorsManagePage;
