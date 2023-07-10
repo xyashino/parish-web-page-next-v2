bcf51132f8b5e7b89ae25ecb1619751fe77c6061
Szczepan M <szczypiorpan123@gmail.com>
2023-07-10 19:30:57 +0200

    Add Admin page and AdminPageTitle component

---
+++ src/app/(admin)/admin/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AdminPageTitle from "@/components/AdminPageTitle";
+
+const Admin = () => {
+  return <AdminPageTitle title="Witaj ponownie" />;
+};
+
+export default Admin;
