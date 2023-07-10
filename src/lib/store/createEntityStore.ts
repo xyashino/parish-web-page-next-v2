d3baf158bb4996ac12c4d23d9ce05821e5940910
Szczepan M <szczypiorpan123@gmail.com>
2023-07-10 19:20:59 +0200

    Add createEntityStore for generic entity management
    Created a new TypeScript file named 'createEntityStore.ts' in 'src/lib/store'. This addition introduces a generic 'EntityStore' which provides basic CRUD operations for any entity type. It also facilitates message customization for various CRUD methods and utilizes the 'zustand' package for state management. The purpose of this addition is to modularize entity management in order to keep the codebase DRY (Don't Repeat Yourself) and improve maintainability.

---
+++ src/lib/store/createEntityStore.ts
@@ -0,0 +1,101 @@
+import { create, createStore } from "zustand";
+import { apiCallWithToast, apiRevalidateCall } from "@/lib/utils";
+import { RevalidateTag } from "@/types/enums/revalidate-tag.enum";
+
+interface Entity {
+  id?: string;
+}
+
+type MethodMessages = {
+  loading?: string;
+  success?: string;
+  error?: string;
+};
+
+type Messages = {
+  createMethod?: MethodMessages;
+  deleteMethod?: MethodMessages;
+  updateMethod?: MethodMessages;
+};
+
+const DEFAULT_MESSAGES: MethodMessages = {
+  loading: "Loading...",
+  success: "Success!",
+  error: "Error!",
+};
+
+interface EntityStore<T extends Entity> {
+  entities: T[];
+  updateAllEntities: (entities: T[]) => void;
+  deleteEntity: (id: T["id"]) => Promise<void>;
+  updateEntity: (entity: T) => Promise<void>;
+  createEntity: (entity: Omit<T, "id">) => Promise<void>;
+}
+
+const customApiCall = <T>(
+  url: string,
+  fetchOptions: RequestInit,
+  messages: any
+) =>
+  apiCallWithToast<T>({
+    url,
+    fetchOptions,
+    msg: { ...DEFAULT_MESSAGES, ...messages },
+  });
+
+export const createEntityStore = <T extends Entity>(
+  defaultValues: T[],
+  baseApiPath: string,
+  revalidateTag: RevalidateTag,
+  messages: Messages = {}
+) => {
+  return create<EntityStore<T>>((set, get) => ({
+    entities: defaultValues,
+
+    updateAllEntities: (entities: T[]) => {
+      set({ entities });
+    },
+
+    createEntity: async (entity: Omit<T, "id">) => {
+      const result = await customApiCall<T>(
+        baseApiPath,
+        {
+          method: "POST",
+          body: JSON.stringify(entity),
+        },
+        messages?.createMethod
+      );
+      set(({ entities }) => ({ entities: [...entities, result] }));
+      return apiRevalidateCall(revalidateTag);
+    },
+
+    updateEntity: async (entityToUpdate: T) => {
+      const result = await customApiCall<T>(
+        `${baseApiPath}/${entityToUpdate.id}`,
+        {
+          method: "PUT",
+          body: JSON.stringify(entityToUpdate),
+        },
+        messages?.updateMethod
+      );
+      set(({ entities }) => ({
+        entities: entities.map((entity) =>
+          entity.id === result.id ? result : entity
+        ),
+      }));
+      return apiRevalidateCall(revalidateTag);
+    },
+
+    deleteEntity: async (id) => {
+      await customApiCall(
+        `${baseApiPath}/${id}`,
+        { method: "DELETE" },
+        messages?.deleteMethod
+      );
+      set(({ entities }) => ({
+        entities: [...entities].filter((entity) => entity.id !== id),
+      }));
+      return apiRevalidateCall(revalidateTag);
+    },
+  }));
+};
