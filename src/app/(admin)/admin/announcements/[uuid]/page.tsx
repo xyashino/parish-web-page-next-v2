cf98e3f384f2fd72b507deeaaa05f8cbd147fb3c
Szczepan M <szczypiorpan123@gmail.com>
2023-07-10 19:26:01 +0200

    Add edit announcements page in admin panel
    A new page for editing announcements has been added to the administrative panel. The page allows an administrator to modify announcements by ID, using getAnnouncements and apiCall to retrieve the specific announcement's details based on its UUID. In case the specified announcement does not exist, a "not found" response is shown to the admin user.

---
+++ src/app/(admin)/admin/announcements/[uuid]/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Announcements } from "@prisma/client";
+import notFound from "@/app/not-found";
+import { getAnnouncements } from "@/lib/prisma/announcement";
+import { apiCall } from "@/lib/utils";
+import AdminPageTitle from "@/components/AdminPageTitle";
+import ModifyAnnouncements from "@/components/ModifyAnnouncements";
+
+export async function generateStaticParams() {
+  const announcements = await getAnnouncements();
+  return announcements.map((announcement) => ({
+    uuid: announcement.id,
+  }));
+}
+
+const EditAnnouncement = async ({ params: { uuid } }: ParamsWithUUID) => {
+  const announcement = await apiCall<Announcements>(
+    `/api/announcements/${uuid}`
+  );
+  if (!announcement) return notFound();
+
+  return (
+    <div className="flex flex-col space-y-6">
+      <AdminPageTitle title={"Edytujesz Ogłoszenia o ID " + uuid} />
+      <ModifyAnnouncements />
+    </div>
+  );
+};
+
+export default EditAnnouncement;
