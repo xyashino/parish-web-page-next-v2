d2a1c49d90a9e5460aa43b476194d65438cf654d
Szczepan M <szczypiorpan123@gmail.com>
2023-07-10 19:22:08 +0200

    "Improve Prisma client usage and export new update-album module"
    The Prisma client has been updated to instantiate only once for production and development environments, preventing unnecessary connections. This improves performance and resource management. Additionally, a new module called 'update-album' has been included in the album folder exports for use in the application.

--- src/lib/prisma/album/index.ts
+++ src/lib/prisma/album/index.ts
@@ -2,3 +2,4 @@ export * from "./delete-album";
 export * from "./create-album";
 export * from "./get-album-with-relation";
 export * from "./get-albums";
+export * from "./update-album";
--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-const client = new PrismaClient();
-export default client;
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+export default prisma;
