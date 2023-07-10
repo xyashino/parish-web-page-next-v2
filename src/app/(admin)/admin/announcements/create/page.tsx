d8aeb20c191d2ddf60e695457ebc207bb8b61ad9
Szczepan M <szczypiorpan123@gmail.com>
2023-07-10 19:26:15 +0200

    Add AnnouncementsCreatePage for admin panel

---
+++ src/app/(admin)/admin/announcements/create/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AdminPageTitle from "@/components/AdminPageTitle";
+import ModifyAnnouncements from "@/components/ModifyAnnouncements";
+const AnnouncementsCreatePage = async () => {
+  return (
+    <div className="flex flex-col space-y-6">
+      <AdminPageTitle title="Dodajesz Ogłoszenia Parafialne" />
+      <ModifyAnnouncements />
+    </div>
+  );
+};
+
+export default AnnouncementsCreatePage;
