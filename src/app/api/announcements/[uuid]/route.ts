35678dd7d90962cdc80adfe48d13983fddac5afe
Szczepan M <szczypiorpan123@gmail.com>
2023-07-10 19:36:22 +0200

    Add new announcement routes
    Added two new routes under the '/announcements' endpoint. The first file 'route.ts' retrieves all the announcements in descending order while the second file '[uuid]/route.ts' fetches a specific announcement using the passed uuid. As of now, Create, Update, and Delete routes remain commented and will be fully implemented in the future.

---
+++ src/app/api/announcements/[uuid]/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { getAnnouncement } from "@/lib/prisma/announcement/get-announcement";
+
+export async function GET(request: Request, { params }: ParamsWithUUID) {
+  const id = params.uuid;
+  const intentions = getAnnouncement(id);
+  return NextResponse.json(intentions);
+}
+//
+// export async function PUT(request: Request, { params }: ParamsWithUuid) {
+//   const id = params.uuid;
+//   const { status, value, subtitle }: Announcements = await request.json();
+//   const intentions = await updateAnnouncement(id, {
+//     value,
+//     subtitle,
+//     status,
+//   });
+//
+//   return NextResponse.json(intentions);
+// }
+//
+// export async function DELETE(request: Request, { params }: ParamsWithUuid) {
+//   const id = params.uuid;
+//   const intentions = await deleteAnnouncement(id);
+//   return NextResponse.json(intentions);
+// }
---
+++ src/app/api/announcements/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { getAnnouncements } from "@/lib/prisma/announcement";
+
+export async function GET() {
+  const result = await getAnnouncements({
+    status: "desc",
+  });
+  return NextResponse.json(result);
+}
+
+// export async function POST(request: Request) {
+//   const { value, status, subtitle }: any = await request.json();
+//
+//   const result = await createAnnouncement({
+//     status,
+//     value,
+//     subtitle: subtitle || null,
+//   });
+//   return NextResponse.json(result);
+// }
