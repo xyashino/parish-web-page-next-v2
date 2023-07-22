3319b4d9729e84a4707b764901e67ad6beafd92b
Szczepan M <szczypiorpan123@gmail.com>
2023-07-22 14:31:39 +0200

    Modify styling rule in IntentionsCreatePage component

--- src/app/(admin)/admin/intentions/create/page.tsx
+++ src/app/(admin)/admin/intentions/create/page.tsx
@@ -4,7 +4,7 @@ import ModifyIntentions from "@/components/ModifyWeekIntentions";
 
 const IntentionsCreatePage = async () => {
   return (
-    <div className="flex flex-col space-y-6 p-4 rounded-xl lg:w-11/12 mx-auto">
+    <div className="flex flex-col space-y-6 p-4 rounded-xl mx-auto">
       <AdminPageTitle title="Dodajesz Intencje Parafialne" />
       <ModifyIntentions />
     </div>
