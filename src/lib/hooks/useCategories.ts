f3b6dd1c047e98358ca039e903837f3ac47562f3
Szczepan M <szczypiorpan123@gmail.com>
2023-07-22 14:21:40 +0200

    Add new hook for category handling

---
+++ src/lib/hooks/useCategories.ts
@@ -0,0 +1,32 @@
+import { Category } from "@prisma/client";
+import { useLayoutEffect, useState } from "react";
+import { apiCall } from "@/lib/utils";
+import { ApiRoute } from "@/types/enums/api-route.enum";
+import { RevalidateTag } from "@/types/enums/revalidate-tag.enum";
+
+export const useCategories = (initialCategories?: Category[]) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useLayoutEffect(() => {
+    if (initialCategories) {
+      setCategories(initialCategories);
+      return;
+    }
+
+    (async () => {
+      setIsLoading(true);
+      const categories = await apiCall<Category[]>(ApiRoute.GET_CATEGORIES, {
+        next: { tags: [RevalidateTag.CATEGORIES] },
+      });
+      setCategories(categories);
+      setIsLoading(false);
+    })();
+  }, [initialCategories]);
+
+  const getCategoryName = (id: Category["id"]) => {
+    const category = categories.find((category) => category.id === id);
+    return category?.name ?? null;
+  };
+  return { isLoading, categories, getCategoryName };
+};
