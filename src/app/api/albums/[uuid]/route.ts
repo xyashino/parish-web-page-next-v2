0f4da2bfe70a3f58bd29bb9e5b4abb36731f6207
Szczepan M <szczypiorpan123@gmail.com>
2023-07-22 14:29:19 +0200

    Update album management and add image display component

--- src/app/api/albums/[uuid]/route.ts
+++ src/app/api/albums/[uuid]/route.ts
@@ -1,22 +1,40 @@
-import { deleteAlbum, updateAlbum } from "@/lib/prisma/album";
+import {
+  deleteAlbum,
+  getAlbumWithRelations,
+  updateAlbum,
+} from "@/lib/db/album";
 import { Album } from "@prisma/client";
 import { NextResponse } from "next/server";
+import { rmdir } from "fs/promises";
+import { join } from "path";
 
-// export async function GET(request: Request, { params }: ParamsWithUUID) {
-//   const id = params.uuid;
-//   // const intentions = getAlbum(id);
-//   return NextResponse.json(intentions);
-// }
-//
+const deleteDirectory = async (id: string) => {
+  const { UPLOAD_DIR } = process.env;
+  if (!UPLOAD_DIR) throw new Error("Upload dir not found");
+  try {
+    await rmdir(join(process.cwd(), UPLOAD_DIR, "/albums", id), {
+      recursive: true,
+    });
+  } catch (error) {
+    throw new Error("Something went wrong");
+  }
+};
+
+export async function GET(request: Request, { params }: ParamsWithUUID) {
+  const id = params.uuid;
+  const album = getAlbumWithRelations(id);
+  return NextResponse.json(album);
+}
+
 export async function PUT(request: Request, { params }: ParamsWithUUID) {
   const id = params.uuid;
   const data: Omit<Album, "id"> = await request.json();
-  const intentions = await updateAlbum(id, data);
-  return NextResponse.json(intentions);
+  const albums = await updateAlbum(id, data);
+  return NextResponse.json(albums);
 }
 export async function DELETE(request: Request, { params }: ParamsWithUUID) {
   const id = params.uuid;
-  console.log("id", id);
-  const intentions = await deleteAlbum(id);
-  return NextResponse.json(intentions);
+  await deleteDirectory(id);
+  const album = await deleteAlbum(id);
+  return NextResponse.json(album);
 }
---
+++ src/components/album/AlbumTabs/albumEditContent/displayAlbumImages/index.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import { ContextMenu } from "@/components/ui/context-menu";
+import { MenuTrigger } from "./menuTrigger";
+import { MenuContextContent } from "./menuContextContent";
+import { Image } from "@prisma/client";
+interface Props {
+  images: Image[];
+}
+
+export const DisplayAlbumImages = ({ images }: Props) => {
+  return (
+    <div className="grid  grid-cols-1 lg:grid-cols-2 gap-6">
+      {images.map(({ id, path }) => {
+        if (!path) return null;
+        return (
+          <div className="flex justify-center" key={id}>
+            <ContextMenu>
+              <MenuTrigger src={path} />
+              <MenuContextContent id={id} />
+            </ContextMenu>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
