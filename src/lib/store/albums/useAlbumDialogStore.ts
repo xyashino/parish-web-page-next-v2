f675debb3145da0cd8b41f8f2afbbf10ad41c932
Szczepan M <szczypiorpan123@gmail.com>
2023-07-22 14:38:17 +0200

    Refactor import statement in album store

--- src/lib/store/albums/useAlbumDialogStore.ts
+++ src/lib/store/albums/useAlbumDialogStore.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { createDialogStore } from "@/lib/store/createDialogStore";
-import modifyAlbumSchema from "@/components/ModifyAlbumDialog/modify-album.schema";
+import { modifyAlbumSchema } from "@/components/ModifyAlbumDialog/modify-album.schema";
 
 const ALBUM_DEFAULT_VALUE: z.infer<typeof modifyAlbumSchema> = {
   title: "",
