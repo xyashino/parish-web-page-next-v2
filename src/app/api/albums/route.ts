2c53c5351ee2f2d6ce06562397353eee157bbaa1
Szczepan M <szczypiorpan123@gmail.com>
2023-07-22 14:29:33 +0200

    Update album creation logic with directory setup

--- src/app/api/albums/route.ts
+++ src/app/api/albums/route.ts
@@ -1,14 +1,20 @@
-// export async function GET() {
-//   const result = await getManyAlbums();
-//   return NextResponse.json(result);
-// }
-//
 import { Album } from "@prisma/client";
-import { createAlbum } from "@/lib/prisma/album";
+import { createAlbum } from "@/lib/db/album";
 import { NextResponse } from "next/server";
+import { mkdir } from "fs/promises";
+import { join } from "path";
+
+const createDirectory = (id: string) => {
+  const { UPLOAD_DIR } = process.env;
+  if (!UPLOAD_DIR) throw new Error("Upload dir not found");
+  return mkdir(join(process.cwd(), UPLOAD_DIR, "/albums", id), {
+    recursive: true,
+  });
+};
 
 export async function POST(request: Request) {
   const data: Omit<Album, "id"> = await request.json();
-  const result = await createAlbum(data);
+  const result = await createAlbum({ ...data });
+  await createDirectory(result.id);
   return NextResponse.json(result);
 }
