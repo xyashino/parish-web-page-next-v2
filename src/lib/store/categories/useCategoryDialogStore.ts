e7e8258937d3a47718567dd26d89c42e4ec97cdb
Szczepan M <szczypiorpan123@gmail.com>
2023-07-22 14:38:26 +0200

    Add 'show' field to CATEGORY_DEFAULT_VALUE in useCategoryDialogStore

--- src/lib/store/categories/useCategoryDialogStore.ts
+++ src/lib/store/categories/useCategoryDialogStore.ts
@@ -1,10 +1,11 @@
-import modifyCategorySchema from "@/components/ModifyCategoryDialog/modify-category.schema";
+import { modifyCategorySchema } from "@/components/ModifyCategoryDialog/modify-category.schema";
 import { createDialogStore } from "@/lib/store/createDialogStore";
 import { z } from "zod";
 
 const CATEGORY_DEFAULT_VALUE: z.infer<typeof modifyCategorySchema> = {
   name: "",
   order: 0,
+  show: true,
 };
 export const useCategoryDialogStore = createDialogStore(
   modifyCategorySchema,
