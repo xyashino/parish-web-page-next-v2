8b1d166fa9ad40657ac7a52581f6a500ed997699
Szczepan M <szczypiorpan123@gmail.com>
2023-07-22 14:32:50 +0200

    Add default 'show' property to modify-category schema

--- src/components/ModifyCategoryDialog/modify-category.schema.ts
+++ src/components/ModifyCategoryDialog/modify-category.schema.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const modifyCategorySchema = z.object({
   name: z.string().nonempty({ message: "Nazwa Kategorii nie może być pusta" }),
   order: z.number().min(-20).max(20).default(0),
+  show: z.boolean().default(false),
 });
 
 export default modifyCategorySchema;
