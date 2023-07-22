8813299baf6ffa69e1f1d0fec17dfaf0414a5720
Szczepan M <szczypiorpan123@gmail.com>
2023-07-22 14:25:57 +0200

    Add missing images info component in album edit

---
+++ src/components/album/AlbumTabs/albumEditContent/missingImagesInfo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const MissingImagesInfo = () => {
+  return (
+    <div>
+      <p className="text-center text-gray-500 text-xl font-bold pt-4">
+        Brak zdjęć do wyświetlenia
+      </p>
+    </div>
+  );
+};
