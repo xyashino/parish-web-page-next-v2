1304ec9ac068443cf75d277e43ed1e6f71f55568
Szczepan M <szczypiorpan123@gmail.com>
2023-07-22 14:30:26 +0200

    Refactor category routes for db changes and remove unused code

--- src/app/api/categories/[uuid]/route.ts
+++ src/app/api/categories/[uuid]/route.ts
@@ -1,25 +1,19 @@
 import { NextResponse } from "next/server";
-import { deleteCategory, updateCategory } from "@/lib/prisma/category";
+import { deleteCategory, updateCategory } from "@/lib/db/category";
 import { Category } from "@prisma/client";
-
-// export async function GET(request: Request, { params }: ParamsWithUuid) {
-//     const id = params.uuid;
-//     const intentions = getCat(id);
-//     return NextResponse.json(intentions);
-// }
 
 export async function PUT(request: Request, { params }: ParamsWithUUID) {
   const id = params.uuid;
   const { name, order }: Category = await request.json();
-  const intentions = await updateCategory(id, {
+  const categories = await updateCategory(id, {
     name,
     order,
   });
-  return NextResponse.json(intentions);
+  return NextResponse.json(categories);
 }
 
 export async function DELETE(request: Request, { params }: ParamsWithUUID) {
   const id = params.uuid;
-  const intentions = await deleteCategory(id);
-  return NextResponse.json(intentions);
+  const category = await deleteCategory(id);
+  return NextResponse.json(category);
 }
