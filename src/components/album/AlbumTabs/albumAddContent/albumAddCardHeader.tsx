ce76a8ee9c83240629218e7e6678d46777a60039
Szczepan M <szczypiorpan123@gmail.com>
2023-07-22 14:23:42 +0200

    Add AlbumAddCardHeader component to AlbumTabs

---
+++ src/components/album/AlbumTabs/albumAddContent/albumAddCardHeader.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { UploadDialog } from "@/components/album/AlbumTabs/albumAddContent/uploadDialog";
+
+export const AlbumAddCardHeader = () => {
+  return (
+    <CardHeader className="flex-row">
+      <div className="grow">
+        <CardTitle>Dodajesz zdjęcia do Albumu</CardTitle>
+        <CardDescription>
+          <span>
+            Pamietaj, kliknąć przycisk
+            <span className="font-bold italic mx-1">Zapisz</span> po dodaniu
+            zdjęć z dysku.
+          </span>
+          <span>
+            Kliknij <span className="font-bold italic">PPM</span> na zdjęcie,
+            aby usunąć je z listy.
+          </span>
+        </CardDescription>
+      </div>
+      <UploadDialog />
+    </CardHeader>
+  );
+};
