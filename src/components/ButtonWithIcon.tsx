1748e3ad91ba80a7160e7aed56d13147539525f4
Szczepan M <szczypiorpan123@gmail.com>
2023-07-27 17:12:53 +0200

    Make the 'text' prop optional in ButtonWithIcon component

--- src/components/ButtonWithIcon.tsx
+++ src/components/ButtonWithIcon.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Button, ButtonProps } from "@/components/ui/button";
 
 interface Props extends ButtonProps {
-  text: string;
+  text?: string;
   Icon: React.ElementType;
 }
 
 const ButtonWithIcon = ({ Icon, text, ...props }: Props) => {
   return (
     <Button {...props}>
-      <Icon className="mr-2 h-4 w-4" /> <span>{text}</span>
+      <Icon className="mr-2 h-4 w-4" /> {text && <span>{text}</span>}
     </Button>
   );
 };
