ceb872ffa34d766f5176a32a53e223ead160183c
Szczepan M <szczypiorpan123@gmail.com>
2023-07-27 15:51:11 +0200

    Add back button to AdminPageTitle component

--- src/components/AdminPageTitle.tsx
+++ src/components/AdminPageTitle.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+"use client";
+import React, { SyntheticEvent } from "react";
 import { Separator } from "@/components/ui/separator";
+import { PinLeftIcon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 interface Props {
   title: string;
 }
 
 const AdminPageTitle = ({ title }: Props) => {
+  const { back } = useRouter();
+  const handleClick = (e: SyntheticEvent) => {
+    e.preventDefault();
+    back();
+  };
+
   return (
-    <div className="w-full space-y-3 p-4 rounded-xl">
-      <h2 className="text-3xl indent-2 italic capitalize font-light text-foreground">
-        {title}
-      </h2>
-      <Separator className="indent-2" />
+    <div className="w-full flex p-4 rounded-xl items-center">
+      <Button variant="ghost" onClick={handleClick}>
+        <PinLeftIcon className="text-foreground text-2xl" />
+      </Button>
+      <div className="w-full  space-y-2">
+        <h2 className="text-3xl indent-2 italic capitalize font-light text-foreground">
+          {title}
+        </h2>
+        <Separator className="indent-2" />
+      </div>
     </div>
   );
 };
