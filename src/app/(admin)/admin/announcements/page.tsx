e566768fea583b7dd376f22078c96b698a4a65f8
Szczepan M <szczypiorpan123@gmail.com>
2023-07-27 15:51:52 +0200

    Add export in index.ts for announcements service

--- src/app/(admin)/admin/announcements/page.tsx
+++ src/app/(admin)/admin/announcements/page.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import AdminPageTitle from "@/components/AdminPageTitle";
 import NavigationBtn from "@/components/navigation/NavigationBtn";
-import { getAnnouncements } from "@/lib/db/announcement";
 import AnnouncementsDataTable from "@/components/data-tables/AnnouncementsDataTable";
+import { apiCall } from "@/lib/utils";
+import { ApiRoute } from "@/types/enums/api-route.enum";
+import { RevalidateTag } from "@/types/enums/revalidate-tag.enum";
+import { Announcements } from "@prisma/client";
+import { PageRoute } from "@/types/enums/page-route.enum";
 
 const AnnouncementsPage = async () => {
-  const announcements = await getAnnouncements();
+  const announcements = await apiCall<Announcements[]>(
+    ApiRoute.BASE_ANNOUNCEMENTS,
+    {
+      next: { tags: [RevalidateTag.ANNOUNCEMENTS] },
+    }
+  );
+
   return (
     <div className="flex flex-col space-y-6">
       <AdminPageTitle title="Zarządzaj ogłoszeniami parafialnymi" />
       <AnnouncementsDataTable data={announcements} />
       <NavigationBtn
-        href="/admin/announcements/create"
+        href={PageRoute.CREATE_ANNOUNCEMENT}
         buttonText="Dodaj ogłosznia"
       />
     </div>
