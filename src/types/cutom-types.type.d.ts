53834869e27e3cfdd46f6b488c0db43ee6e24981
Szczepan M <szczypiorpan123@gmail.com>
2023-07-27 15:51:27 +0200

    Add "ApiMethod" type to custom-types.type
    To streamline work with API and ensure type safety, a distinct type "ApiMethod" was added into the `custom-types.type.d.ts` file. This type includes all possible HTTP verbs, improving code readability and maintainability.

--- src/types/cutom-types.type.d.ts
+++ src/types/cutom-types.type.d.ts
@@ -1,3 +1,5 @@
 type OptionalID<T extends { id: string }> = Omit<T, "id"> & { id?: string };
 
 type ParamsWithUUID = { params: { uuid: string } };
+
+type ApiMethod = "POST" | "PUT" | "DELETE" | "GET" | "PATCH";
