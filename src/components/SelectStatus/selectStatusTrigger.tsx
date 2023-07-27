736e0e490711251ed21f3c18fc6190ce48418335
Szczepan M <szczypiorpan123@gmail.com>
2023-07-27 15:04:30 +0200

    Change SelectStatus placeholder value to "BRAK"

--- src/components/SelectStatus/selectStatusTrigger.tsx
+++ src/components/SelectStatus/selectStatusTrigger.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Status } from "@prisma/client";
 
 interface Props {
@@ -14,7 +9,7 @@ interface Props {
 const SelectStatusTrigger = ({ value }: Props) => {
   return (
     <SelectTrigger className="uppercase min-w-[200px]" value={value}>
-      <SelectValue placeholder="Status" />
+      <SelectValue placeholder="BRAK" />
     </SelectTrigger>
   );
 };
