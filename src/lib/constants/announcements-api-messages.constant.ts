83fac8fde4395003dd14afe4d5e70d170410c595
Szczepan M <szczypiorpan123@gmail.com>
2023-07-27 15:00:46 +0200

    Add API messages for announcements
    A new file 'announcements-api-messages.constant.ts' was added in the 'constants' directory. This file includes success, error, and loading messages related to the creation, update, and deletion of announcements.

---
+++ src/lib/constants/announcements-api-messages.constant.ts
@@ -0,0 +1,17 @@
+export const ANNOUNCEMENTS_API_MESSAGES = {
+  delete: {
+    loading: "Usuwanie ogłoszeń...",
+    success: "Ogłoszenie zostało usunięte",
+    error: "Nie udało się usunąć ogłoszenia",
+  },
+  update: {
+    loading: "Zapisywanie zmian...",
+    success: "Zmiany zostały zapisane",
+    error: "Nie udało się zapisać zmian",
+  },
+  create: {
+    loading: "Tworzenie ogłoszenia...",
+    success: "Ogłoszenie zostało utworzone",
+    error: "Nie udało się utworzyć ogłoszenia",
+  },
+} as const;
