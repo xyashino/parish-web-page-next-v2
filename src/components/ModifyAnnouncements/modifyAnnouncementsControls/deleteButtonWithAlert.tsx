de3c80bdef641d423de1348369dd921480126fe4
Szczepan M <szczypiorpan123@gmail.com>
2023-07-27 15:48:01 +0200

    Add delete announcement feature

---
+++ src/components/ModifyAnnouncements/modifyAnnouncementsControls/deleteButtonWithAlert.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ConfirmAlert from "@/components/ConfirmAlert";
+import { TrashIcon } from "@radix-ui/react-icons";
+
+interface Props {
+  onConfirm: () => void;
+}
+
+const triggerValue = (
+  <>
+    <TrashIcon className="mr-2 h-4 w-4" /> Usuń
+  </>
+);
+
+export const DeleteButtonWithAlert = ({ onConfirm }: Props) => (
+  <ConfirmAlert
+    headerData={{
+      title: "Czy na pewno chcesz usunąć ogłoszenia ?",
+      description:
+        "Zmiany zostaną wykoname trwale. Nie będzie można ich przywrócić.",
+    }}
+    triggerData={{
+      triggerValue,
+      className: "w-1/4 bg-red-500 hover:bg-red-600 transition-colors",
+    }}
+    footerData={{
+      doAfterConfirm: onConfirm,
+      confirmText: "Usuń",
+    }}
+  />
+);
---
+++ src/components/ModifyAnnouncements/modifyAnnouncementsControls/index.tsx
@@ -0,0 +1,47 @@
+import React, { SyntheticEvent } from "react";
+import ButtonWithIcon from "@/components/ButtonWithIcon";
+import { UpdateIcon } from "@radix-ui/react-icons";
+import CardHeaderWithSeparator from "@/components/CardHeaderWithSeparator";
+import { Card, CardContent } from "@/components/ui/card";
+import { ClearButtonWithAlert } from "./clearButtonWithAlert";
+import { DeleteButtonWithAlert } from "./deleteButtonWithAlert";
+
+interface Props {
+  saveMethod: () => void;
+  clearMethod: () => void;
+  isEdit: boolean;
+  deleteMethod: () => void;
+}
+
+const ModifyAnnouncementsControls = ({
+  saveMethod,
+  clearMethod,
+  deleteMethod,
+  isEdit,
+}: Props) => {
+  const handleSave = (e: SyntheticEvent) => {
+    e.preventDefault();
+    saveMethod();
+  };
+  return (
+    <Card className="w-11/12 mx-auto">
+      <CardHeaderWithSeparator
+        title="Zarządzaj ogłoszeniami"
+        description="Na stronie głównej wyświetlane są tylko jede aktywne ogłoszenia. Które
+        sa wybierane losowo."
+      />
+      <CardContent className="flex justify-around">
+        <ClearButtonWithAlert onConfirm={clearMethod} />
+        <ButtonWithIcon
+          text="Zapisz"
+          Icon={UpdateIcon}
+          className="w-1/4"
+          onClick={handleSave}
+        />
+        {isEdit && <DeleteButtonWithAlert onConfirm={deleteMethod} />}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ModifyAnnouncementsControls;
