944892bc7580366e9cbefb425e870469953f2f27
xyashino <szczypiorpan123@gmail.com>
2023-08-08 15:02:45 +0200

    Add new routes to ApiRoute enum
    Enum ApiRoute was extended with three new routes: BASE_ALBUMS, BASE_WEEK_INTENTIONS, and BASE_IMAGES. These routes were added to manage respective resources in an app more efficiently.

--- src/types/enums/api-route.enum.ts
+++ src/types/enums/api-route.enum.ts
@@ -1,5 +1,8 @@
 export enum ApiRoute {
   UPLOAD_IMAGE = "/api/upload/image",
   BASE_CATEGORIES = "/api/categories",
+  BASE_ALBUMS = "/api/albums",
   BASE_ANNOUNCEMENTS = "/api/announcements",
+  BASE_WEEK_INTENTIONS = "/api/intentions",
+  BASE_IMAGES = "/api/images",
 }
