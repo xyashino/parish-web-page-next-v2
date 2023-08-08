dab50f0927a131732c7a6208706765e8173bae30
xyashino <szczypiorpan123@gmail.com>
2023-08-08 15:10:19 +0200

    Simplify admin navigation routes and types

--- src/lib/constants/admin-navigation-routes.constant.tsx
+++ src/lib/constants/admin-navigation-routes.constant.tsx
@@ -1,78 +1,30 @@
-import { NavigationItemType } from "@/types/interfaces/navigation.interface";
+import { SingleNavigationItem } from "@/types/interfaces/navigation.interface";
 import {
-  ListBulletIcon,
   ImageIcon,
-  ArchiveIcon,
   PersonIcon,
-  DashboardIcon,
   ChatBubbleIcon,
   FileTextIcon,
-  PlusCircledIcon,
-  Pencil2Icon,
 } from "@radix-ui/react-icons";
 
-export const ADMIN_NAVIGATION_ROUTES: NavigationItemType[] = [
+export const ADMIN_NAVIGATION_ROUTES: SingleNavigationItem[] = [
   {
     href: "/admin/intentions",
     text: "Intencje",
     icon: <ChatBubbleIcon />,
-    nestedRoutes: [
-      {
-        href: "",
-        text: "Wszystkie",
-        icon: <ListBulletIcon />,
-      },
-      {
-        href: "/create",
-        text: "Utwórz",
-        icon: <PlusCircledIcon />,
-      },
-    ],
   },
   {
     href: "/admin/announcements",
     text: "Ogłoszenia",
     icon: <FileTextIcon />,
-    nestedRoutes: [
-      {
-        href: "",
-        text: "Wszystkie",
-        icon: <ListBulletIcon />,
-      },
-      {
-        href: "/create",
-        text: "Utwórz",
-        icon: <PlusCircledIcon />,
-      },
-    ],
   },
   {
     href: "/admin/gallery",
     text: "Galeria",
     icon: <ImageIcon />,
-    nestedRoutes: [
-      {
-        href: "/categories",
-        text: "Katagorie",
-        icon: <DashboardIcon />,
-      },
-      {
-        href: "/albums",
-        text: "Albumy",
-        icon: <ArchiveIcon />,
-      },
-    ],
   },
   {
     href: "/admin/administrators",
     text: "Administratorzy",
     icon: <PersonIcon />,
-    nestedRoutes: [
-      {
-        href: "/manage",
-        text: "Zarządzaj",
-        icon: <Pencil2Icon />,
-      },
-    ],
   },
 ];
