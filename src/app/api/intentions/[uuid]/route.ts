61655b7f0355870f1e83028f42e56fddd39de603
xyashino <szczypiorpan123@gmail.com>
2023-08-08 15:12:46 +0200

    Add PUT and DELETE operations to week intentions

--- src/app/api/intentions/[uuid]/route.ts
+++ src/app/api/intentions/[uuid]/route.ts
@@ -1,5 +1,11 @@
 import { NextResponse } from "next/server";
-import { getWeekIntentionWithRelations } from "@/lib/db/weekIntentions";
+import {
+  deleteWeekIntention,
+  getWeekIntentionWithRelations,
+} from "@/lib/db/weekIntentions";
+import { weekUpdateIntentionsValidator } from "@/lib/validators";
+import { deleteDaysByWeekId } from "@/lib/db/day";
+import { updateWeekIntentions } from "@/lib/db/weekIntentions/update-week-intentions";
 
 export async function GET(request: Request, { params }: ParamsWithUUID) {
   const id = params.uuid;
@@ -7,32 +13,32 @@ export async function GET(request: Request, { params }: ParamsWithUUID) {
   return NextResponse.json(intentions);
 }
 
-// export async function PUT(request: Request, { params }: any) {
-//   const id = params.uuid;
-//   const { days, ...restOfWeekIntentionsData }: WeekIntentionsData =
-//     await request.json();
-//
-//   await deleteIntention(id);
-//
-//   const intentions = await createIntention({
-//     ...restOfWeekIntentionsData,
-//     days: {
-//       create: days.map(({ intentions, ...restOfDayIntentions }) => ({
-//         ...restOfDayIntentions,
-//         intentions: {
-//           create: intentions.map((intention) => ({
-//             ...intention,
-//           })),
-//         },
-//       })),
-//     },
-//   });
-//
-//   return NextResponse.json(intentions);
-// }
-//
-// export async function DELETE(request: Request, { params }: ParamsWithUuid) {
-//   const id = params.uuid;
-//   const intentions = await deleteIntention(id);
-//   return NextResponse.json(intentions);
-// }
+export async function PUT(request: Request, { params }: any) {
+  const id = params.uuid;
+  const data = await request.json();
+  const { days, ...weekData } = weekUpdateIntentionsValidator.parse(data);
+
+  await deleteDaysByWeekId(id);
+
+  const intentions = await updateWeekIntentions(id, {
+    ...weekData,
+    days: {
+      create: days.map(({ intentions, ...restOfDayIntentions }) => ({
+        ...restOfDayIntentions,
+        intentions: {
+          create: intentions.map((intention) => ({
+            ...intention,
+          })),
+        },
+      })),
+    },
+  });
+
+  return NextResponse.json(intentions);
+}
+
+export async function DELETE(request: Request, { params }: ParamsWithUUID) {
+  const id = params.uuid;
+  const intention = await deleteWeekIntention(id);
+  return NextResponse.json(intention);
+}
