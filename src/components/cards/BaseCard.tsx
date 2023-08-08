423791cf3a0da30be0f7c751644d533e4a1f220c
xyashino <szczypiorpan123@gmail.com>
2023-08-08 15:23:14 +0200

    Add BaseCard component to standardize card layout
    Created a new BaseCard component in 'src/components/cards/'. This component was created to provide a uniform style and structure for all cards used across the application. The BaseCard takes a title and optional icon as props, offering flexibility while maintaining consistency.

---
+++ src/components/cards/BaseCard.tsx
@@ -0,0 +1,27 @@
+import React, {
+  ForwardRefExoticComponent,
+  HTMLAttributes,
+  PropsWithChildren,
+  RefAttributes,
+} from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { IconProps } from "@radix-ui/react-icons/dist/types";
+
+interface Props extends PropsWithChildren, HTMLAttributes<HTMLDivElement> {
+  title: string;
+  Icon?: ForwardRefExoticComponent<IconProps & RefAttributes<SVGSVGElement>>;
+}
+
+const BaseCard = ({ children, title, Icon, ...props }: Props) => {
+  return (
+    <Card {...props}>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="font-bold">{title}</CardTitle>
+        {Icon && <Icon className="ml-4 h-4 w-4 text-muted-foreground" />}
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+    </Card>
+  );
+};
+
+export default BaseCard;
