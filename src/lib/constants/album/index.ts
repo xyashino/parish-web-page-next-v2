53ff3cb69d77a5dabf4145814e8cdb237eab4d40
xyashino <szczypiorpan123@gmail.com>
2023-08-08 15:18:34 +0200

    Add album constants with default value and API messages
    A new file src/lib/constants/album/index.ts has been created. This file introduces default value for album object and standardized API messages for create, delete and update operations on albums. This change was necessary to streamline the handling of album objects and to provide more consistent user feedback during CRUD operations.

---
+++ src/lib/constants/album/index.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+import { modifyAlbumSchema } from "@/lib/schemas/album";
+import { ApiToastsCrudMessages } from "@/types/toast";
+
+export const ALBUM_DEFAULT_VALUE: z.infer<typeof modifyAlbumSchema> = {
+  title: "",
+  subtitle: "",
+  categoryId: "",
+  show: true,
+};
+
+export const ALBUM_API_MESSAGES: ApiToastsCrudMessages = {
+  create: {
+    loading: "Tworzenie albumu...",
+    success: "Album został utworzony",
+    error: "Nie udało się utworzyć albumu",
+  },
+  delete: {
+    loading: "Usuwanie albumu...",
+    success: "Album został usunięty",
+    error: "Nie udało się usunąć albumu",
+  },
+  update: {
+    loading: "Aktualizowanie albumu...",
+    success: "Album został zaktualizowany",
+    error: "Nie udało się zaktualizować albumu",
+  },
+} as const;
