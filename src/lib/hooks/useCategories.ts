4623a0133736cd5fb653908a36b317d202ee9595
xyashino <szczypiorpan123@gmail.com>
2023-08-08 15:09:12 +0200

    Refactor useCategories hook to simplify data fetching

--- src/lib/hooks/useCategories.ts
+++ src/lib/hooks/useCategories.ts
@@ -3,22 +3,15 @@ import { useLayoutEffect, useState } from "react";
 import { apiCall } from "@/lib/utils";
 import { ApiRoute } from "@/types/enums/api-route.enum";
 import { RevalidateTag } from "@/types/enums/revalidate-tag.enum";
-import { useCategoriesStore } from "@/lib/store";
 
 export const useCategories = (initialCategories?: Category[]) => {
-  const { entities } = useCategoriesStore();
   const [isLoading, setIsLoading] = useState(false);
   const [categories, setCategories] = useState<Category[]>([]);
 
   useLayoutEffect(() => {
-    if (initialCategories || entities.length > 0) {
-      setCategories(initialCategories || entities);
-      return;
-    }
-
     (async () => {
       setIsLoading(true);
-      const categories = await apiCall<Category[]>(ApiRoute.GET_CATEGORIES, {
+      const categories = await apiCall<Category[]>(ApiRoute.BASE_CATEGORIES, {
         next: { tags: [RevalidateTag.CATEGORIES] },
       });
       setCategories(categories);
