8dc75d5f098a7cfe99ce6923f16c641731e34df9
xyashino <szczypiorpan123@gmail.com>
2023-08-08 15:11:49 +0200

    Refactor admin sidebar structure for improved user experience
    The AdminNavigationSideBar component was significantly refactored. Largely, these changes were made to enhance the administrators' navigation user experience. The new component now includes a more user-friendly and flexible layout. Also, a logout alert and a logo, represented within their own components, have been added to the sidebar, increasing the comprehensibility and overall usability of the navigation. Changes were also supplemented with the necessary adjustments in related files.

---
+++ src/components/navigation/AdminNavigationSideBar/adminSidebarLogo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "@/components/Logo";
+import { Navigation } from "@/types/enums/navigation.enum";
+
+export const AdminSidebarLogo = () => {
+  return (
+    <div className="p-5 space-y-1 bg-foreground">
+      <Logo
+        className="hover:bg-transparent font-bold p-0 text-background"
+        navigateTo={Navigation.ADMIN_HOME}
+      />
+    </div>
+  );
+};
---
+++ src/components/navigation/AdminNavigationSideBar/index.tsx
@@ -0,0 +1,30 @@
+import React, { HTMLAttributes } from "react";
+import { cn } from "@/lib/utils";
+import { ADMIN_NAVIGATION_ROUTES } from "@/lib/constants/admin-navigation-routes.constant";
+import NavigationLink from "@/components/navigation/NavigationLink";
+import { LogoutAlert } from "@/components/navigation/AdminNavigationSideBar/logoutAlert";
+import { AdminSidebarLogo } from "@/components/navigation/AdminNavigationSideBar/adminSidebarLogo";
+
+interface SidebarProps extends HTMLAttributes<HTMLDivElement> {}
+
+const AdminNavigationSideBar = ({ className }: SidebarProps) => (
+  <div className={cn("pb-12 w-1/6 flex flex-col", className)}>
+    <AdminSidebarLogo />
+    <div className="grow">
+      <div className="px-3 py-2">
+        <div className="space-y-1">
+          {ADMIN_NAVIGATION_ROUTES.map(({ text, icon, href }, i) => (
+            <NavigationLink
+              href={href}
+              text={text}
+              icon={icon}
+              key={`${text}-${i}`}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+    <LogoutAlert />
+  </div>
+);
+export default AdminNavigationSideBar;
