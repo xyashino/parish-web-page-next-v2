0404136b31a5b081680c687449eee20200a65bc0
xyashino <szczypiorpan123@gmail.com>
2023-08-08 15:03:05 +0200

    Refactored Toast messages configuration in api-call-with-toast
    A refactor was done in the api-call-with-toast file. The changes include the importation of a DEFAULT_API_TOAST_MESSAGES constant and using it as the default value of the 'msg' argument.

--- src/lib/utils/api-call-with-toast.ts
+++ src/lib/utils/api-call-with-toast.ts
@@ -1,28 +1,20 @@
+import { DEFAULT_API_TOAST_MESSAGES } from "@/lib/constants/common";
+import { ToastMessages } from "@/types/toast";
 import { apiCall } from "@/lib/utils/api-call";
-import toast, { Renderable, ValueOrFunction } from "react-hot-toast";
+import toast from "react-hot-toast";
 
 interface FetchDataWithToastOptions<T> {
   url: string;
   fetchOptions?: RequestInit;
   overrideOptions?: boolean;
-  msg?: {
-    loading: Renderable;
-    success: ValueOrFunction<Renderable, T>;
-    error: ValueOrFunction<Renderable, any>;
-  };
+  msg?: ToastMessages;
 }
-
-const DEFAULT_MSG = {
-  loading: "Wczytywanie...",
-  success: "Sukces!",
-  error: "Upps... Coś poszło nie tak :/",
-};
 
 export const apiCallWithToast = async <T>({
   url,
   fetchOptions,
   overrideOptions,
-  msg = DEFAULT_MSG,
+  msg = DEFAULT_API_TOAST_MESSAGES,
 }: FetchDataWithToastOptions<T>) => {
   const resultPromise = apiCall<T>(url, fetchOptions, overrideOptions);
   toast.promise(resultPromise, msg);
