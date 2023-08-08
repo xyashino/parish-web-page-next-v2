a18b6cdcd3fc7e08a1b24a90763858f470960fbc
xyashino <szczypiorpan123@gmail.com>
2023-08-08 22:30:12 +0200

    Refactor useUploadImages hook to improve readability
    Refactored the useUploadImages hook by separating the api call into a separate function named uploadImageApiCall for better readability and easier testability. Introduced two new hooks, useParams and useRouter, to fetch the uuid and to refresh the page respectively once the images are uploaded.

--- src/lib/hooks/useUploadImages.ts
+++ src/lib/hooks/useUploadImages.ts
@@ -2,46 +2,53 @@ import { useState } from "react";
 import { Image } from "@prisma/client";
 import { apiCall } from "@/lib/utils";
 import { useUploadImagesStore } from "@/lib/store/useUploadImagesStore";
-import { useUploadedImagesStore } from "@/lib/store/useUploadedImagesStore";
 import { useProgressStore } from "@/lib/store/useProgressStore";
 import { ApiRoute } from "@/types/enums/api-route.enum";
+import { useParams, useRouter } from "next/navigation";
+
+const uploadImageApiCall = async (image: File, uuid: string) => {
+  const formData = new FormData();
+  formData.append(uuid, image);
+  return await apiCall<Image>(
+    ApiRoute.UPLOAD_IMAGE,
+    {
+      method: "POST",
+      body: formData,
+      headers: {},
+    },
+    true
+  );
+};
 
 export const useUploadImages = () => {
-  const { clearImages } = useUploadImagesStore();
+  const { uuid } = useParams();
+  const { clearImages, images } = useUploadImagesStore();
   const { value, setMaxValue, setCurrentTrack, reset } = useProgressStore();
-  const { addImage } = useUploadedImagesStore();
+  const { refresh } = useRouter();
 
   const [isUploading, setIsUploading] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  const uploadImages = async (images: File[], uuid: string) => {
+  const resetStates = () => {
+    setIsDialogOpen(false);
+    clearImages();
+    setTimeout(() => {
+      reset();
+      setIsUploading(false);
+    }, 1000);
+  };
+
+  const uploadImages = async () => {
     let index = 0;
     setMaxValue(images.length);
     setIsUploading(true);
 
     for (const image of images) {
-      const formData = new FormData();
-      formData.append(uuid, image);
-      const uploadedImage = await apiCall<Image>(
-        ApiRoute.UPLOAD_IMAGE,
-        {
-          method: "POST",
-          body: formData,
-          headers: {},
-        },
-        true
-      );
-      addImage(uploadedImage);
+      await uploadImageApiCall(image, uuid);
       setCurrentTrack(++index);
     }
-
-    setIsDialogOpen(false);
-    clearImages();
-
-    setTimeout(() => {
-      reset();
-      setIsUploading(false);
-    }, 1000);
+    resetStates();
+    refresh();
   };
 
   return {
