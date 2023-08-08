31d9440701d59da7ec48e9483d9391c457595e41
xyashino <szczypiorpan123@gmail.com>
2023-08-08 15:14:28 +0200

    Rename type file and add NullToUndefined
    The file "custom-types.type.d.ts" has been renamed to "common.d.ts" to more accurately reflect its use case. Along with the renaming, an additional utility type "NullToUndefined" has been added to assist with nullability issues during the development process.

--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -3,3 +3,7 @@ type OptionalID<T extends { id: string }> = Omit<T, "id"> & { id?: string };
 type ParamsWithUUID = { params: { uuid: string } };
 
 type ApiMethod = "POST" | "PUT" | "DELETE" | "GET" | "PATCH";
+
+type NullToUndefined<T> = {
+  [P in keyof T]: T[P] extends null ? undefined : T[P];
+};
