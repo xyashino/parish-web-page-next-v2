a62f169412f89c631595b70cbf34db375befe276
xyashino <szczypiorpan123@gmail.com>
2023-08-14 14:44:32 +0200

    Refactor Album page to use apiCall function
    Switched from using getAlbumWithRelations to use apiCall function in the Admin Album page. This was done to ensure consistency and readability of the code. The apiCall function is used across the pages to retrieve data which adheres to our aim for encapsulated, reusable code. This includes changes to AlbumTabs and AlbumInfoCard to accommodate for the switch.

--- src/app/(admin)/admin/albums/[uuid]/page.tsx
+++ src/app/(admin)/admin/albums/[uuid]/page.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import notFound from "@/app/not-found";
-import { getAlbums, getAlbumWithRelations } from "@/lib/db/album";
+import { getAlbums } from "@/lib/db/album";
 import { AdminPageTitle } from "@/components/AdminPageTitle";
 import { AlbumInfoCard } from "@/components/album/AlbumInfoCard";
 import { AlbumCoverImageCard } from "@/components/album/AlbumCoverImageCard";
 import { AlbumTabs } from "@/components/album/AlbumTabs";
+import { apiCall } from "@/lib/utils";
+import { ApiRoute } from "@/types/enums/api-route.enum";
+import { RevalidateTag } from "@/types/enums/revalidate-tag.enum";
+import { Album, Category, Image } from "@prisma/client";
+
+type AlbumWithRelations = Album & {
+  images: Image[] | null;
+  category: Category | null;
+};
 
 export async function generateStaticParams() {
   const announcements = await getAlbums();
@@ -14,19 +23,23 @@ export async function generateStaticParams() {
 }
 
 const MenageAlbum = async ({ params: { uuid } }: ParamsWithUUID) => {
-  const { images, ...album } = await getAlbumWithRelations(uuid);
+  const { images, ...album } = await apiCall<AlbumWithRelations>(
+    `${ApiRoute.BASE_ALBUMS}/${uuid}`,
+    {
+      next: { tags: [RevalidateTag.IMAGES, RevalidateTag.ALBUMS] },
+    }
+  );
 
   if (!album) return notFound();
-
   return (
     <div className="flex flex-col space-y-2">
       <AdminPageTitle title={`Album: "${album.title}"`} />
       <div className="mx-4 w-full lg:w-11/12 lg:mx-auto space-y-4">
         <div className="w-full justify-around flex">
-          <AlbumInfoCard albumData={album} />
+          <AlbumInfoCard albumData={album} images={images ?? []} />
           <AlbumCoverImageCard imageCoverId={album.coverId} />
         </div>
-        <AlbumTabs />
+        <AlbumTabs images={images ?? []} />
       </div>
     </div>
   );
