3e2e7f46ef7ddbf868fd033783eafafb6dd7a9e2
xyashino <szczypiorpan123@gmail.com>
2023-08-14 14:48:39 +0200

    Refactor album directory creation to a separate service
    The function to create a new directory for every new album was previously in the route file, which was cluttering the route file and not following the separation of concerns principle. This function has been moved to a new server-methods file under the services/albums directory. This makes the routing code more readable and maintainable, and allows reusability of the directory creation function.

--- src/app/api/albums/route.ts
+++ src/app/api/albums/route.ts
@@ -1,18 +1,10 @@
 import { Album } from "@prisma/client";
 import { createAlbum, getAlbums } from "@/lib/db/album";
 import { NextResponse } from "next/server";
-import { mkdir } from "fs/promises";
-import { join } from "path";
 import { revalidateTag } from "next/cache";
 import { RevalidateTag } from "@/types/enums/revalidate-tag.enum";
+import { createDirectory } from "@/lib/services/albums/server-methods";
 
-const createDirectory = (id: string) => {
-  const { UPLOAD_DIR } = process.env;
-  if (!UPLOAD_DIR) throw new Error("Upload dir not found");
-  return mkdir(join(process.cwd(), UPLOAD_DIR, "/albums", id), {
-    recursive: true,
-  });
-};
 export async function GET() {
   const albums = await getAlbums();
   return NextResponse.json(albums);
