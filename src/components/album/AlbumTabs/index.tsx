d28e79749bfc5b9f0481fc07f6d3834496948a92
xyashino <szczypiorpan123@gmail.com>
2023-08-14 14:51:42 +0200

    "Add images prop to AlbumTabs component
    The AlbumTabs component now accepts an "images" prop of type Image[]. This change allows the AlbumEditContent to display the actual images from the album."

--- src/components/album/AlbumTabs/index.tsx
+++ src/components/album/AlbumTabs/index.tsx
@@ -1,22 +1,23 @@
 import React from "react";
+import { Image } from "@prisma/client";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { AlbumTabsValue } from "./album-tabs-value.enum";
 import { AlbumAddContent } from "./albumAddContent";
 import { AlbumEditContent } from "./albumEditContent";
 
-export const AlbumTabs = () => {
-  return (
-    <div>
-      <Tabs defaultValue={AlbumTabsValue.ADD_IMAGES}>
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value={AlbumTabsValue.ADD_IMAGES}>Dodaj</TabsTrigger>
-          <TabsTrigger value={AlbumTabsValue.EDIT_IMAGES}>
-            Przeglądaj
-          </TabsTrigger>
-        </TabsList>
-        <AlbumAddContent />
-        <AlbumEditContent />
-      </Tabs>
-    </div>
-  );
-};
+interface Props {
+  images: Image[];
+}
+
+export const AlbumTabs = ({ images }: Props) => (
+  <div>
+    <Tabs defaultValue={AlbumTabsValue.ADD_IMAGES}>
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value={AlbumTabsValue.ADD_IMAGES}>Dodaj</TabsTrigger>
+        <TabsTrigger value={AlbumTabsValue.EDIT_IMAGES}>Przeglądaj</TabsTrigger>
+      </TabsList>
+      <AlbumAddContent />
+      <AlbumEditContent images={images} />
+    </Tabs>
+  </div>
+);
