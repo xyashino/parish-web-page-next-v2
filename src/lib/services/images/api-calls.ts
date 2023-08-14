0d8bee68067a3aa1b4d0f833f11248fd0fdda899
xyashino <szczypiorpan123@gmail.com>
2023-08-14 14:43:34 +0200

    Add image management API calls in 'api-calls.ts'
    This commit adds new functions to handle API calls related to image management. Functions have been added for fetching an image's cover path, updating an album's cover image, uploading a new image, and deleting an image. These changes provide a structured way to interact with the server and manage image data. They provide application-wide standard methods for corresponding server operations, improving code readability and maintainability.

---
+++ src/lib/services/images/api-calls.ts
@@ -0,0 +1,47 @@
+import { apiCall, apiCallWithToast } from "@/lib/utils";
+import { Album, Image } from "@prisma/client";
+import { ApiRoute } from "@/types/enums/api-route.enum";
+import { UPDATE_COVER_IMAGE_MESSAGES } from "@/lib/constants/image";
+
+export const getImageCoverPath = async (id: string) => {
+  return (await apiCall<Image>(`${ApiRoute.BASE_IMAGES}/${id}`)).path;
+};
+
+export const updateCoverImage = async (albumId: string, coverId: string) => {
+  return apiCallWithToast<Image>({
+    url: `${ApiRoute.BASE_ALBUMS}/${albumId}`,
+    fetchOptions: {
+      method: "PUT",
+      body: JSON.stringify({ coverId } as Partial<Album>),
+    },
+    msg: UPDATE_COVER_IMAGE_MESSAGES,
+  });
+};
+
+export const uploadImageApiCall = async (image: File, uuid: string) => {
+  const formData = new FormData();
+  formData.append(uuid, image);
+  return await apiCall<Image>(
+    ApiRoute.UPLOAD_IMAGE,
+    {
+      method: "POST",
+      body: formData,
+      headers: {},
+    },
+    true
+  );
+};
+
+export const deleteImageApiCall = async (id: string) => {
+  return apiCallWithToast<Image>({
+    url: `${ApiRoute.BASE_IMAGES}/${id}`,
+    fetchOptions: {
+      method: "DELETE",
+    },
+    msg: {
+      success: "Zdjęcie zostało usunięte",
+      error: "Błąd podczas usuwania zdjęcia",
+      loading: "Usuwanie zdjęcia...",
+    },
+  });
+};
