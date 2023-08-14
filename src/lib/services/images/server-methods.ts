6eb4731585456b47bf9958b64c24b353f140aa81
xyashino <szczypiorpan123@gmail.com>
2023-08-14 14:46:01 +0200

    Refactor image upload and handling structure
    Renamed and moved 'route-helper.ts' to a more centralized location in '/lib/services/images'. The code has been updated to use node:fs and node:path instead of the initially used path in order to align with the updated Node.js methodology.
    
    The function 'checkDirectory' has been expanded to check for the existence of a specific album directory, instead of just the default upload directory. A new environment variable UPLOAD_DIR_ALBUM has been added to accommodate this change.
    
    In 'validateImage', the ignored parameter in the destructuring operation is specifically expressed as '_key', to increase code readability.
    
    Finally, a new function 'clearImage' has been added to allow for easy deletion of images from the server. This is intended to provide better data management and potentially faster processing times.

--- src/lib/services/images/server-methods.ts
+++ src/lib/services/images/server-methods.ts
@@ -1,13 +1,17 @@
-import { getAlbumWithRelations } from "@/lib/db/album";
-import { mkdir, readdir, writeFile } from "fs/promises";
-import { join } from "path";
+"use server";
 import sharp from "sharp";
+import { unlink, mkdir, readdir, writeFile } from "node:fs/promises";
+import { join } from "node:path";
+import { deleteImage } from "@/lib/db/image";
+import { getAlbumWithRelations } from "@/lib/db/album";
 
-const { UPLOAD_DIR } = process.env;
+const { UPLOAD_DIR, UPLOAD_DIR_ALBUM } = process.env;
+
 export const checkDirectory = async (path: string) => {
-  if (!UPLOAD_DIR) throw new Error("Upload dir not found , check .env file");
+  if (!UPLOAD_DIR || !UPLOAD_DIR_ALBUM)
+    throw new Error("Upload dir not found , check .env file");
   try {
-    await readdir(join(process.cwd(), UPLOAD_DIR, path));
+    await readdir(join(process.cwd(), UPLOAD_DIR, UPLOAD_DIR_ALBUM, path));
   } catch (error) {
     await mkdir(join(process.cwd(), UPLOAD_DIR, path), { recursive: true });
   }
@@ -16,12 +20,12 @@ export const checkDirectory = async (path: string) => {
 export const checkAlbum = async (id: string) => {
   const album = await getAlbumWithRelations(id);
   if (!album) throw new Error("Album not found");
-  await checkDirectory(`public/albums/${album.id}`);
+  await checkDirectory(album.id);
   return album;
 };
 
 export const validateImage = (file: [string, File | string]) => {
-  const [key, value] = file;
+  const [_key, value] = file;
   if (typeof value === "string") throw new Error("File is not an image");
   const { type, size } = value;
   if (!type.includes("image")) throw new Error("File is not an image");
@@ -29,6 +33,7 @@ export const validateImage = (file: [string, File | string]) => {
 
   return value;
 };
+
 export const validateFormData = (formData: [string, File | string][]) => {
   if (formData.length !== 1) throw new Error("Only one file allowed");
 
@@ -39,10 +44,24 @@ export const validateFormData = (formData: [string, File | string][]) => {
     file,
   };
 };
+
 export const saveAsWebp = async (buffer: ArrayBuffer, path: string) => {
   let pathCopy = path;
   if (!pathCopy.endsWith(".webp")) pathCopy = pathCopy + ".webp";
   if (!UPLOAD_DIR) throw new Error("Upload dir not found , check .env file");
   const webpBuffer = await sharp(buffer).webp().toBuffer();
-  await writeFile(join(process.cwd(), UPLOAD_DIR, `${path}`), webpBuffer);
+  await writeFile(join(process.cwd(), UPLOAD_DIR, `${pathCopy}`), webpBuffer);
+};
+
+export const clearImage = async (id: string) => {
+  if (!UPLOAD_DIR) throw new Error("Upload dir not found");
+  try {
+    const deletedImage = await deleteImage(id);
+    await unlink(
+      join(process.cwd(), UPLOAD_DIR, deletedImage.path ?? "test.png")
+    );
+    return deletedImage;
+  } catch (error) {
+    throw new Error("Something went wrong");
+  }
 };
