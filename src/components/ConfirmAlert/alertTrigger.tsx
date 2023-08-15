6a7a35d4254ef9129dec7181c7ec6892ea88d937
xyashino <szczypiorpan123@gmail.com>
2023-08-15 20:21:37 +0200

    Refactor AlertTrigger to use buttonVariants
    This commit refactors the AlertTrigger component to use the buttonVariants function for styling rather than an inline string. The aim of this change is to enhance code readability and maintainability by reusing predefined styles in

--- src/components/ConfirmAlert/alertTrigger.tsx
+++ src/components/ConfirmAlert/alertTrigger.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { cn } from "@/lib/utils";
 import { AlertDialogTriggerProps } from "@radix-ui/react-alert-dialog";
+import { buttonVariants } from "@/components/ui/button";
 
 export interface AlertTriggerProps extends AlertDialogTriggerProps {
   triggerValue: React.ReactNode;
@@ -16,9 +17,7 @@ export const AlertTrigger = ({
   ...props
 }: AlertTriggerProps) => {
   const triggerClasses = cn(
-    disableAllClasses
-      ? ""
-      : "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 w-1/4",
+    disableAllClasses ? "" : buttonVariants({ variant: "default" }),
     className
   );
   return (
