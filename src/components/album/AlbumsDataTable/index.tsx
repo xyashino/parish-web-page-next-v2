05781e87a73fa8065d7ace8d5ec670c59045a77d
xyashino <szczypiorpan123@gmail.com>
2023-08-15 19:30:48 +0200

    Simplify AlbumsDataTable component
    This commit removes the unused 'Separator' and reduces unnecessary markup in the AlbumsDataTable component. By directly using DataTable and passing the table title as a property, the component is now simpler and easier to maintain. This change improves code readability, promotes code reuse, and reduces potential places for errors to occur.

--- src/components/album/AlbumsDataTable/index.tsx
+++ src/components/album/AlbumsDataTable/index.tsx
@@ -3,21 +3,14 @@ import React, { PropsWithChildren } from "react";
 import { DataTable } from "@/components/DataTable";
 import { Album } from "@prisma/client";
 import { albumColumns } from "./album-columns";
-import { Separator } from "@/components/ui/separator";
 
 interface Props extends PropsWithChildren {
   data: Album[];
 }
-export const AlbumsDataTable = ({ data }: Props) => {
-  return (
-    <div>
-      <div className="w-11/12 mx-auto space-y-2 mb-2">
-        <h3 className="text-foreground text-2xl font-bold">
-          Wszystkie Albumy:
-        </h3>
-        <Separator />
-      </div>
-      <DataTable columns={albumColumns} data={data} />
-    </div>
-  );
-};
+export const AlbumsDataTable = ({ data }: Props) => (
+  <DataTable
+    tableTitle="Wszystkie Albumy:"
+    columns={albumColumns}
+    data={data}
+  />
+);
