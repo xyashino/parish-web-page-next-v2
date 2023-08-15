bb43fb151b1db9cfb6ecd28e44e116cf6ee848d3
xyashino <szczypiorpan123@gmail.com>
2023-08-15 19:29:36 +0200

    Refactor CategoriesDataTable to streamline props
    This change simplifies the CategoriesDataTable component by passing the 'tableTitle' directly as a prop. We are eliminating redundant div elements and boosting readability and maintainability of our code. Also, this commit aligns with our effort to make our code more DRY (Don't Repeat Yourself).

--- src/components/categories/CategoriesDataTable/index.tsx
+++ src/components/categories/CategoriesDataTable/index.tsx
@@ -3,22 +3,15 @@ import React, { PropsWithChildren } from "react";
 import { DataTable } from "@/components/DataTable";
 import { Category } from "@prisma/client";
 import { categoryColumns } from "./category-columns";
-import { Separator } from "@/components/ui/separator";
 
 interface Props extends PropsWithChildren {
   data: Category[];
 }
 
-export const CategoriesDataTable = ({ data }: Props) => {
-  return (
-    <div>
-      <div className="w-11/12 mx-auto space-y-2 mb-2">
-        <h3 className="text-foreground text-2xl font-bold">
-          Wszystkie Kategorie:
-        </h3>
-        <Separator />
-      </div>
-      <DataTable columns={categoryColumns} data={data} />
-    </div>
-  );
-};
+export const CategoriesDataTable = ({ data }: Props) => (
+  <DataTable
+    tableTitle="Wszystkie Kategorie:"
+    columns={categoryColumns}
+    data={data}
+  />
+);
