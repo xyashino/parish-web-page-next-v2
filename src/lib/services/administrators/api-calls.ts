0fa61afe595b44a44254690f8a8c239fce14d392
xyashino <szczypiorpan123@gmail.com>
2023-08-15 20:19:35 +0200

    Add administrator API calls
    This commit introduces two new API calls related to the administrator's account management. The first one is used for creating a new administrator account, and the second one deletes an existing administrator account. This change will enable the front-end application to interact with the back-end system to manage administrator's accounts.

---
+++ src/lib/services/administrators/api-calls.ts
@@ -0,0 +1,31 @@
+import { apiCallWithToast } from "@/lib/utils";
+import { ApiRoute } from "@/types/enums/api-route.enum";
+
+export const createAdministratorApiCall = async (values: { email: string }) => {
+  await apiCallWithToast({
+    url: ApiRoute.BASE_ADMINISTRATORS,
+    fetchOptions: {
+      method: "POST",
+      body: JSON.stringify(values),
+    },
+    msg: {
+      success: "Pomyślnie dodano administratora.",
+      error: "Nie udało się dodać administratora.",
+      loading: "Trwa dodawanie administratora...",
+    },
+  });
+};
+
+export const deleteAdministratorApiCall = async (id: string) => {
+  await apiCallWithToast({
+    url: `${ApiRoute.BASE_ADMINISTRATORS}/${id}`,
+    fetchOptions: {
+      method: "DELETE",
+    },
+    msg: {
+      success: "Pomyślnie usunięto administratora.",
+      error: "Nie udało się usunąć administratora.",
+      loading: "Trwa usuwanie administratora...",
+    },
+  });
+};
