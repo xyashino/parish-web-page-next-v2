f5fcc6055363a50cc97111d037ea68cd633a6495
xyashino <szczypiorpan123@gmail.com>
2023-08-15 19:19:36 +0200

    Add public exports for DataTable sub-components
    This commit introduces public exports for DataTable sub-components in the index.ts file. It includes DataTableActionsDropDown, DataTableActionsDropDownItem, DataTableBooleanCell, DataTableSortBtn and DataTableStatusCell. These changes provide accessibility for use across the application.

---
+++ src/components/DataTable/public/index.ts
@@ -0,0 +1,5 @@
+export * from "./DataTableActionsDropDown";
+export * from "./DataTableActionsDropDownItem";
+export * from "./DataTableBooleanCell";
+export * from "./DataTableSortBtn";
+export * from "./DataTableStatusCell";
