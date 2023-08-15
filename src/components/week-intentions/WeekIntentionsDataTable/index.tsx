ba3f19bed0444e514f0f94759df29499db579515
xyashino <szczypiorpan123@gmail.com>
2023-08-15 19:29:18 +0200

    Refactor WeekIntentionsDataTable component
    The WeekIntentionsDataTable component has been refactored for simplicity and better reuse of components. The DataTable component now directly takes the title as a prop instead of using a separate div for the title and separator. This makes the WeekIntentionsDataTable more readable and follows DRY principles better. This change doesn't affect the features or the user interface of the application.

--- src/components/week-intentions/WeekIntentionsDataTable/index.tsx
+++ src/components/week-intentions/WeekIntentionsDataTable/index.tsx
@@ -3,22 +3,15 @@ import React, { PropsWithChildren } from "react";
 import { DataTable } from "@/components/DataTable";
 import { WeekIntentions } from "@prisma/client";
 import { intentionsColumns } from "./intentions-columns";
-import { Separator } from "@/components/ui/separator";
 
 interface Props extends PropsWithChildren {
   data: WeekIntentions[];
 }
 
-export const WeekIntentionsDataTable = ({ data }: Props) => {
-  return (
-    <div>
-      <div className="w-11/12 mx-auto space-y-2 mb-2">
-        <h3 className="text-foreground text-2xl font-bold">
-          Wszystkie Intencje Parafialne:
-        </h3>
-        <Separator />
-      </div>
-      <DataTable columns={intentionsColumns} data={data} />
-    </div>
-  );
-};
+export const WeekIntentionsDataTable = ({ data }: Props) => (
+  <DataTable
+    tableTitle="Wszystkie Intencje Parafialne:"
+    columns={intentionsColumns}
+    data={data}
+  />
+);
