d0a17795ca8079c1d9dfdcb31f0a0d57051f2e22
xyashino <szczypiorpan123@gmail.com>
2023-08-15 19:28:25 +0200

    Add delete administrator route
    A new route for deleting an administrator by UUID has been added in the administrators api. The important point here is revalidation of cache which takes place after administrator deletion. On successful deletion, a JSON response will be returned.

---
+++ src/app/api/administrators/[uuid]/route.ts
@@ -0,0 +1,11 @@
+import { deleteAdministrator } from "@/lib/db/administrators";
+import { revalidateTag } from "next/cache";
+import { RevalidateTag } from "@/types/enums/revalidate-tag.enum";
+import { NextResponse } from "next/server";
+
+export async function DELETE(request: Request, { params }: ParamsWithUUID) {
+  const id = params.uuid;
+  const album = await deleteAdministrator(id);
+  revalidateTag(RevalidateTag.ADMINISTRATORS);
+  return NextResponse.json(album);
+}
