582ccf1f22b5c2c550d4b123b6a70b50c375489d
xyashino <szczypiorpan123@gmail.com>
2023-08-15 19:37:22 +0200

    Add ProviderButton component for social logins
    Added a new component, ProviderButton, to handle social logins via Google and Facebook. This button provides the user with the ability to sign in using their social media account. It maintains its own loading state to manage user interaction while the sign-in process is ongoing. This simplifies the social login process and improves the user experience.

---
+++ src/components/login/ProviderButton.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { ReactNode, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { Navigation } from "@/types/enums/navigation.enum";
+
+interface Props {
+  provider: "google" | "facebook";
+  buttonText: string;
+  icon?: ReactNode;
+}
+
+export const ProviderButton = ({ provider, buttonText, icon }: Props) => {
+  const { NEXT_PUBLIC_APP_URL } = process.env;
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    setLoading(true);
+    signIn(provider, {
+      callbackUrl:
+        NEXT_PUBLIC_APP_URL ?? "http://localhost:3000" + Navigation.ADMIN_HOME,
+      redirect: false,
+    });
+  };
+
+  return (
+    <Button
+      className="text-sm space-x-2"
+      variant="outline"
+      type="button"
+      onClick={handleClick}
+      disabled={loading}
+    >
+      {icon && <div>{icon}</div>}
+      <span>{buttonText}</span>
+    </Button>
+  );
+};
