6d09ac6dda455b8b73e9a49658535ce8463bd5e3
xyashino <szczypiorpan123@gmail.com>
2023-08-15 19:34:33 +0200

    Add login page and its components
    A new LoginPage component has been added to implement the login functionality. The component checks for a valid session and, if found, redirects to the Admin Home page. It uses a LeftSection and a RightSection for its structure and styling. The Metadata object provides the title and description for the page.

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+import { RightSection } from "@/components/login/RightSection";
+import { LeftSection } from "@/components/login/LeftSection";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { Navigation } from "@/types/enums/navigation.enum";
+
+export const metadata: Metadata = {
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+};
+
+export default async function AuthenticationPage() {
+  const session = await getServerSession(authOptions);
+  if (session) redirect(Navigation.ADMIN_HOME);
+  return (
+    <div className="w-full h-full">
+      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <LeftSection />
+        <RightSection />
+      </div>
+    </div>
+  );
+}
