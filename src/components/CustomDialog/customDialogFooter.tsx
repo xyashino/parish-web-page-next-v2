3383a24418296ba1fa4599546fdf38847db69426
xyashino <szczypiorpan123@gmail.com>
2023-08-15 19:24:13 +0200

    Add "submitText" prop to CustomDialogFooter
    The "CustomDialogFooter" component has been updated to receive a new prop: "submitText". This is an optional string used to customize the button's text. If this prop isn't provided, the default text "Zapisz" will be used. This change offers more flexibility for using this component in different contexts or languages.

--- src/components/CustomDialog/customDialogFooter.tsx
+++ src/components/CustomDialog/customDialogFooter.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { DialogFooter } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 
-export const CustomDialogFooter = () => {
-  return (
-    <DialogFooter>
-      <Button type="submit">Zapisz</Button>
-    </DialogFooter>
-  );
-};
+interface Props {
+  submitText?: string;
+}
+
+export const CustomDialogFooter = ({ submitText }: Props) => (
+  <DialogFooter>
+    <Button type="submit" className="mt-2">
+      {submitText || "Zapisz"}
+    </Button>
+  </DialogFooter>
+);
