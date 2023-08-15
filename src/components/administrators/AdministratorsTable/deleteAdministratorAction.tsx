18f7bcb6b1e6d1ef196b5de695b5495c1912b9fa
xyashino <szczypiorpan123@gmail.com>
2023-08-15 20:20:50 +0200

    Add delete administrator functionality
    Added a new file deleteAdministratorAction.tsx under the administrators component, incorporating the functionality to delete an administrator from the list. It includes the handleDelete function making API call to delete an administrator by its ID. After successful deletion, page refreshes to update the list. Also, to make sure deleting is intentional, a confirm alert has been added before the action is performed

---
+++ src/components/administrators/AdministratorsTable/deleteAdministratorAction.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { buttonVariants } from "@/components/ui/button";
+import { TrashIcon } from "@radix-ui/react-icons";
+import { deleteAdministratorApiCall } from "@/lib/services/administrators/api-calls";
+import { useRouter } from "next/navigation";
+import { ConfirmAlert } from "@/components/ConfirmAlert";
+
+interface Props {
+  id: string;
+}
+
+const DeleteAdministratorAction = ({ id }: Props) => {
+  const { refresh } = useRouter();
+
+  const handleDelete = async () => {
+    await deleteAdministratorApiCall(id);
+    refresh();
+  };
+  return (
+    <ConfirmAlert
+      headerData={{
+        title: "Czy na pewno chcesz usunąć administratora?",
+        description: "Emial utraci dostęp do logowania.",
+      }}
+      triggerData={{
+        triggerValue: <TrashIcon />,
+        className: buttonVariants({ variant: "destructive" }),
+      }}
+      footerData={{
+        confirmText: "Usuń",
+        doAfterConfirm: handleDelete,
+      }}
+    />
+  );
+};
+
+export default DeleteAdministratorAction;
