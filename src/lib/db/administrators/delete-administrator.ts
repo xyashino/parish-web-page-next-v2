779ce90d467dc9b058117f0ac6a289fe528253ea
xyashino <szczypiorpan123@gmail.com>
2023-08-15 20:20:36 +0200

    Change deleteAdministrator to use id instead of email
    Administrator deletion has been updated to use the id of the administrator rather than the email. This change ensures a more precise and error

--- src/lib/db/administrators/delete-administrator.ts
+++ src/lib/db/administrators/delete-administrator.ts
@@ -1,8 +1,8 @@
 import client from "@/lib/db";
 
-export const deleteAdministrator = async (email: string) => {
+export const deleteAdministrator = async (id: string) => {
   try {
-    return await client.admin.delete({ where: { email } });
+    return await client.admin.delete({ where: { id } });
   } catch (error) {
     console.error(error);
     throw new Error("Something went wrong");
