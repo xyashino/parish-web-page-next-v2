42f05f0f95c3ce5ef4b3d62982021a336e7b0e9d
xyashino <szczypiorpan123@gmail.com>
2023-08-15 19:28:50 +0200

    Add API call and components to Administrators Page
    This commit includes the addition of an API call to fetch a list of administrators on the 'AdministratorsManagePage'. Also added the 'AdministratorsTable' and 'AddAdministratorDialog' components to the 'AdministratorsManagePage' to manage administrators more efficiently. This aids in enhancing the user interface and allows admins to manage other administrators from a single page.

--- src/app/(admin)/admin/administrators/page.tsx
+++ src/app/(admin)/admin/administrators/page.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { AdminPageTitle } from "@/components/AdminPageTitle";
+import { apiCall } from "@/lib/utils";
+import { RevalidateTag } from "@/types/enums/revalidate-tag.enum";
+import { Admin } from "@prisma/client";
+import { ApiRoute } from "@/types/enums/api-route.enum";
+import {
+  AddAdministratorDialog,
+  AdministratorsTable,
+} from "@/components/administrators";
 
 const AdministratorsManagePage = async () => {
+  const administrators = await apiCall<Admin[]>(ApiRoute.BASE_ADMINISTRATORS, {
+    next: { tags: [RevalidateTag.ADMINISTRATORS] },
+  });
+
   return (
-    <div className="flex flex-col space-y-6">
+    <div className="flex flex-col">
       <AdminPageTitle title="Zarządzaj Administatorami" />
+      <AdministratorsTable data={administrators} />
+      <AddAdministratorDialog />
     </div>
   );
 };
