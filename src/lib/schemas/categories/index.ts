a7218d69515adb50bd0c14d3787afc0e42fa2926
xyashino <szczypiorpan123@gmail.com>
2023-08-16 13:44:23 +0200

    Change default category visibility to true
    Changed the default value of the 'show' attribute in 'modifyCategorySchema' to true. This modification reflects the need for categories to be visible by default for enhanced user experience.

--- src/lib/schemas/categories/index.ts
+++ src/lib/schemas/categories/index.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const modifyCategorySchema = z.object({
   name: z.string().nonempty({ message: "Nazwa Kategorii nie może być pusta" }),
   order: z.number().min(-20).max(20).default(0),
-  show: z.boolean().default(false),
+  show: z.boolean().default(true),
 });
