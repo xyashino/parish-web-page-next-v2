db57b8b5e4284eeb4cf479521f82e1e704d6b353
xyashino <szczypiorpan123@gmail.com>
2023-08-16 13:29:05 +0200

    Add customizable separator classes to CardHeaderWithSeparator
    The `CardHeaderWithSeparator` component was extended to allow customization of the separator's classes. This change ensures that the styles of the separator can be controlled from the parent component, thereby increasing the reusability and flexibility of the `CardHeaderWithSeparator` component. The class names provided will be concatenated to the default 'w-11/12 mx-auto'.

--- src/components/cards/CardHeaderWithSeparator.tsx
+++ src/components/cards/CardHeaderWithSeparator.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
 
 interface Props {
   title: string;
   description?: string;
+  separatorClasses?: string;
 }
 
-export const CardHeaderWithSeparator = ({ title, description }: Props) => {
+export const CardHeaderWithSeparator = ({
+  title,
+  description,
+  separatorClasses,
+}: Props) => {
+  const separatorClassesDefault = cn("w-11/12 mx-auto", separatorClasses);
+
   return (
     <CardHeader>
       <CardTitle>{title}</CardTitle>
       {description && <CardDescription>{description}</CardDescription>}
-      <Separator className="w-11/12 mx-auto" />
+      <Separator className={separatorClassesDefault} />
     </CardHeader>
   );
 };
