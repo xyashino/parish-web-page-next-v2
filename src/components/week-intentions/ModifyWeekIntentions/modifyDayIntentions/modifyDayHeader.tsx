7bbb49974355cdfc5daeaa2ba046cdc509924739
xyashino <szczypiorpan123@gmail.com>
2023-08-18 12:57:14 +0200

    Refactor ModifyDayHeader component for code readability
    The ModifyDayHeader component was updated for better structuring and readability. The React import statement was modified to take in ChangeEvent to directly import it, instead of directly calling React.ChangeEvent. Input from "./components/ui/input" was included

--- src/components/week-intentions/ModifyWeekIntentions/modifyDayIntentions/modifyDayHeader.tsx
+++ src/components/week-intentions/ModifyWeekIntentions/modifyDayIntentions/modifyDayHeader.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import { useWeekIntentionsStore } from "@/lib/store/useWeekIntentionsStore";
 import { DayIntentions } from "@/types/interfaces/week-intentions-store.interface";
 import { weekdayTranslator } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
 
 export const ModifyDayHeader = ({
   day,
   dateOfDay,
 }: Omit<DayIntentions, "intentions">) => {
   const { updateDay } = useWeekIntentionsStore();
-  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleDateChange = (e: ChangeEvent<HTMLInputElement>) => {
     const date = new Date(e.target.value);
     updateDay(date);
   };
@@ -16,9 +17,9 @@ export const ModifyDayHeader = ({
   return (
     <div className="flex w-full items-center justify-between rounded-t-2xl px-8 py-4 text-sm font-bold text-slate-50 bg-slate-950 md:text-xl">
       <h3 className="mr-2 uppercase">{weekdayTranslator.get(day)}</h3>
-      <input
+      <Input
         type="date"
-        className="rounded-md text-slate-950 md:p-2 text-xs font-light"
+        className="rounded-md text-slate-950 md:p-2 text-xs font-light max-w-[140px] uppercase font-mono"
         value={dateOfDay ? new Date(dateOfDay).toISOString().slice(0, 10) : ""}
         onChange={handleDateChange}
       />
