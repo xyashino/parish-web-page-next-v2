1a3aa5c886ca0a5768220f9d83bb6c623f349ddb
xyashino <szczypiorpan123@gmail.com>
2023-08-18 12:50:14 +0200

    Reduce padding and enhance text readability on mobile
    Decreased the padding and made the font size responsive in the AdminPageTitle component. This was necessary to ensure optimal visibility and accommodate the layout on smaller, mobile screens, enhancing overall mobile responsiveness.

--- src/components/AdminPageTitle.tsx
+++ src/components/AdminPageTitle.tsx
@@ -17,12 +17,12 @@ export const AdminPageTitle = ({ title }: Props) => {
   };
 
   return (
-    <div className="w-full  p-6 space-y-4 rounded-xl  flex-wrap">
+    <div className="w-full p-2 lg:p-6 space-y-2 lg:space-y-4 rounded-xl  flex-wrap">
       <div className="flex items-center space-x-2.5">
         <Button variant="ghost" onClick={handleClick}>
           <PinLeftIcon className="text-foreground text-2xl" />
         </Button>
-        <h2 className="text-2xl font-bold capitalize text-foreground ">
+        <h2 className="text-xl lg:text-2xl font-bold capitalize text-foreground  text-center">
           {title}
         </h2>
       </div>
