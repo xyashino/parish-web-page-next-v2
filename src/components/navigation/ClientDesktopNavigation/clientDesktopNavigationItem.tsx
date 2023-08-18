75faa7dfb27c531c5f88a1be2e702ef819e2dbbc
xyashino <szczypiorpan123@gmail.com>
2023-08-18 15:12:08 +0200

    Add ClientDesktopNavigationItem component
    This commit introduces the ClientDesktopNavigationItem component. This component accepts a title and href as props and creates a navigational link. This change was made to modularize the code and organize the client desktop navigation structure efficiently.

---
+++ src/components/navigation/ClientDesktopNavigation/clientDesktopNavigationItem.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+import {
+  NavigationMenuItem,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { NavigationItem } from "@/types/interfaces/navigation.interface";
+
+export const ClientDesktopNavigationItem = ({
+  title,
+  href,
+}: NavigationItem) => {
+  return (
+    <NavigationMenuItem>
+      <Link href={href} legacyBehavior passHref>
+        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+          {title}
+        </NavigationMenuLink>
+      </Link>
+    </NavigationMenuItem>
+  );
+};
