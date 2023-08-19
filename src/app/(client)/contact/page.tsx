cc3e70e19d8f97842865448f4a332ada80ac2410
xyashino <szczypiorpan123@gmail.com>
2023-08-19 16:25:07 +0200

    "Add new Contact page component"
    In this commit, a new Contact page component is added to the client application. This component includes a ContactHeader, a Google Map, and lists out contact information from a defined data set. This was needed to provide users with contact details and location.

---
+++ src/app/(client)/contact/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ContactHeader } from "@/components/contact/ContactHeader";
+import { ContactGoogleMap } from "@/components/contact/ContactGoogleMap";
+import { ContactItem } from "@/components/contact/ContactItem";
+import { CONTACT_DATA } from "@/config/contact";
+
+export default function Contact() {
+  return (
+    <section className="bg-background h-full my-auto">
+      <div className="container px-6 py-12 mx-auto">
+        <ContactHeader />
+        <div className="grid grid-cols-1 gap-12 mt-6 lg:grid-cols-3">
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-1">
+            {CONTACT_DATA.map((data, i) => (
+              <ContactItem key={`${data.title}-${i}`} {...data} />
+            ))}
+          </div>
+          <ContactGoogleMap />
+        </div>
+      </div>
+    </section>
+  );
+}
