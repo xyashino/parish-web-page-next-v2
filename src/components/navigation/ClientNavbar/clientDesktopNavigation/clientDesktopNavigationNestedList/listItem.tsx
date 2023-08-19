2490990b218c18d27fcc90ac80e774122133fc9f
xyashino <szczypiorpan123@gmail.com>
2023-08-19 16:20:53 +0200

    "Refactor className assignment in listItem.tsx
    The initial application of className in listItem.tsx was adjusted to be more streamlined and concise. The className attribute is now assigned directly to the 'li' HTML element instead of within the 'a' tag. This results in cleaner, less redundant code which should improve readability and maintainability of the component moving forward."

--- src/components/navigation/ClientNavbar/clientDesktopNavigation/clientDesktopNavigationNestedList/listItem.tsx
+++ src/components/navigation/ClientNavbar/clientDesktopNavigation/clientDesktopNavigationNestedList/listItem.tsx
@@ -7,13 +7,12 @@ export const ListItem = React.forwardRef<
   React.ComponentPropsWithoutRef<"a">
 >(({ className, title, children, ...props }, ref) => {
   return (
-    <li>
+    <li className={className}>
       <NavigationMenuLink asChild>
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
           )}
           {...props}
         >
