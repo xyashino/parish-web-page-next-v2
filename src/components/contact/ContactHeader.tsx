96dfeb24233a387c73b99e8b5089ee8e8e9c79ea
xyashino <szczypiorpan123@gmail.com>
2023-08-19 16:24:15 +0200

    Add ContactHeader component in contact page
    A new ContactHeader.tsx file is created under src/components/contact to improve user interaction. This component includes the header of the contact page and encourages users to make contact if they have any questions or doubts. The update ensures better communication with users, which in turn enhances user engagement on the site.

---
+++ src/components/contact/ContactHeader.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const ContactHeader = () => {
+  return (
+    <div>
+      <p className="font-medium text-destructive">Skontaktuj się z nami</p>
+
+      <h1 className="mt-2 text-2xl font-semibold text-gray-800 md:text-3xl dark:text-white">
+        Nawiąż kontakt
+      </h1>
+
+      <p className="mt-3 text-gray-500 dark:text-gray-400">
+        Skontaktuj się z nami w razie jakichkolwiek pytań lub wątpliwości.
+      </p>
+    </div>
+  );
+};
