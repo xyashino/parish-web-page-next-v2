2d19dc516aa076c7144fce9433ab2d709fee6dd9
xyashino <szczypiorpan123@gmail.com>
2023-08-19 16:24:05 +0200

    Add Google Map to contact component
    A new file "ContactGoogleMap.tsx" was created in the "components/contact" directory. This includes a Google Map iframe embed code for display on the Contact page of the website. This modification improves the user's comprehension of the location and adds value to the Contact page.

---
+++ src/components/contact/ContactGoogleMap.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const ContactGoogleMap = () => {
+  return (
+    <div className="overflow-hidden rounded-lg lg:col-span-2 h-96 lg:h-auto">
+      <iframe
+        title="Mapa Parafi Gruszów"
+        allowFullScreen={false}
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        width="100%"
+        height="100%"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10217.207956565826!2d21.0334529!3d50.1928859!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x43123091c4e284d7!2sKo%C5%9Bci%C3%B3%C5%82%20%C5%9Bw.%20Maksymiliana%20Kolbe%20w%20Gruszowie!5e0!3m2!1sen!2spl!4v1672533479972!5m2!1sen!2spl"
+        className="w-full mx-auto rounded-xl bg-slate-50 p-4 shadow-xl h-full"
+      />
+    </div>
+  );
+};
