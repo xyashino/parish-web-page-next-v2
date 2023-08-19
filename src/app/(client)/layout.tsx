66e1a9a3fcb69683900a45e5ca628937b54eb910
xyashino <szczypiorpan123@gmail.com>
2023-08-19 16:18:32 +0200

    Replace ClientDesktopNavigation with ClientNavbar
    The ClientDesktopNavigation has been replaced with ClientNavbar in layout.tsx. This change was made to introduce a more suitable navigation component for all screen sizes and improve the overall user interface. The CSS for the main section has also been tweaked to adapt to this change.

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -1,12 +1,12 @@
 import React, { PropsWithChildren } from "react";
 import { Footer } from "@/components/Footer";
-import { ClientDesktopNavigation } from "@/components/navigation/ClientDesktopNavigation";
+import { ClientNavbar } from "@/components/navigation/ClientNavbar";
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex flex-col h-screen w-screen max-w-full overflow-hidden">
-      <ClientDesktopNavigation />
-      <main className="grow flex flex-col justify-between overflow-hidden  overflow-y-scroll   bg-muted">
+      <ClientNavbar />
+      <main className="flex-grow flex flex-col justify-between overflow-hidden overflow-y-scroll   bg-muted relative z-10">
         {children}
         <Footer showAdminIcon />
       </main>
