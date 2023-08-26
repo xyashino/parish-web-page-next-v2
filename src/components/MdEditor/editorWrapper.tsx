25e767257c5a987676ae369fc626c524601a5754
xyashino <szczypiorpan123@gmail.com>
2023-08-26 17:25:14 +0200

    Add Polish support and styles to the Markdown Editor
    Two new import statements were added to `editorWrapper.tsx` - one for the Polish language package and another for the basic CSS file of the Toast UI Editor. This change introduces Polish language support and applies necessary styles for the Markdown Editor component.

--- src/components/MdEditor/editorWrapper.tsx
+++ src/components/MdEditor/editorWrapper.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Editor, EditorProps } from "@toast-ui/react-editor";
+import "@toast-ui/editor/dist/i18n/pl-pl";
+import "@toast-ui/editor/dist/toastui-editor.css";
 
 export interface EditorWrapperForwardedProps extends EditorProps {
   forwardedRef?: React.MutableRefObject<Editor>;
