75bbe3f760278304ee49bb24ca087e60a5135b75
xyashino <szczypiorpan123@gmail.com>
2023-08-26 17:29:21 +0200

    Add WeekIntentionsTabContent component
    A new functional component, WeekIntentionsTabContent, was added in the week-intentions directory. This component receives the day, the date of the day, intentions, and id as props. Some of the imported modules include TabsContent and weekdayTranslator. The component renders the day and date in the header and maps through the intentions to present them in the front-end. This was implemented to help better manage the weekly intentions presentation by condensing the related functionality into a single component.

---
+++ src/components/week-intentions/WeekIntentionsContent/weekIntentionsTabContent.tsx
@@ -0,0 +1,48 @@
+import { TabsContent } from "@/components/ui/tabs";
+import { Day, Intention } from "@prisma/client";
+import { weekdayTranslator } from "@/lib/utils";
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+interface Props extends Day {
+  intentions: Intention[];
+}
+export const WeekIntentionsTabContent = ({
+  day,
+  dateOfDay,
+  intentions,
+  id,
+}: Props) => {
+  return (
+    <TabsContent
+      value={id}
+      className="mx-auto mt-4 w-full rounded-2xl shadow "
+      id={`${id}`}
+    >
+      <header className="flex flex-col md:flex-row items-center justify-center space-y-2 md:space-y-0 md:space-x-4 text-3xl text-background italic bg-foreground p-4 rounded-t select-none">
+        <h2 className="font-extrabold text-center">
+          {weekdayTranslator.get(day)?.toUpperCase()}
+        </h2>
+        <span className="font-light text-2xl text-center">
+          {dateOfDay && new Date(dateOfDay).toLocaleDateString("pl-PL")}
+        </span>
+      </header>
+
+      <div className="flex flex-col">
+        {intentions.map((intention) => (
+          <div
+            key={intention.id}
+            className="flex flex-col md:flex-row w-full justify-around items-center p-4 border-2 divide-y-2 md:divide-y-0 md:divide-x-2"
+          >
+            <h5 className="text-2xl font-extrabold uppercase italic text-center w-full md:w-1/6">
+              <span className="md:hidden font-medium">Godzina:</span>{" "}
+              {intention.hour}
+            </h5>
+            <div className="w-full md:w-5/6 px-8 p-2 mx-auto prose prose-sm max-w-none">
+              <MDXRemote source={intention.value} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </TabsContent>
+  );
+};
