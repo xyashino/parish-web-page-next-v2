cf2b3224657c5ad1434ad4e39b565ee74f99342e
xyashino <szczypiorpan123@gmail.com>
2023-08-26 17:27:10 +0200

    Add WeekIntentionsContent component
    Added a new component, WeekIntentionsContent, for managing daily intentions. This component uses a tabs system to organize and display intentions for each day of the week. It uses the 'weekdayTranslator' utility to represent days, and the 'getActiveDayID' utility to synchronize with the current day. In the list of tabs ('TabsList') and its content, unique IDs from 'crypto.randomUUID()' are used for avoiding collision.

---
+++ src/components/week-intentions/WeekIntentionsContent/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Day, Intention } from "@prisma/client";
+import { Tabs, TabsList } from "@/components/ui/tabs";
+import { getActiveDayID, weekdayTranslator } from "@/lib/utils";
+import { WeekIntentionsTabContent } from "./weekIntentionsTabContent";
+import { CustomTriggerWithScroll } from "./customTriggerWithScroll";
+
+interface Props {
+  days: (Day & {
+    intentions: Intention[];
+  })[];
+}
+
+const TAB_LIST_ID = crypto.randomUUID();
+
+export const WeekIntentionsContent = ({ days }: Props) => {
+  return (
+    <Tabs defaultValue={getActiveDayID(days)}>
+      <TabsList
+        className="flex h-auto w-full flex-wrap items-center justify-center"
+        id={`${TAB_LIST_ID}`}
+      >
+        {days.map(({ day, id }) => (
+          <CustomTriggerWithScroll
+            id={id}
+            key={id}
+            translatedDay={weekdayTranslator.get(day) ?? day}
+            tabID={TAB_LIST_ID}
+          />
+        ))}
+      </TabsList>
+
+      {days.map((day) => (
+        <WeekIntentionsTabContent key={day.id} {...day} />
+      ))}
+    </Tabs>
+  );
+};
