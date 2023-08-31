4c12a9877566e7f2d8aeb261dae56b0582d29ca8
xyashino <szczypiropan123@gmial.com>
2023-08-31 20:56:47 +0200

    Add vertical margin to main content container
    The main content container's CSS class in layout.tsx was modified to include vertical margins. This was done to improve overall layout aesthetics and to ensure content is not flush against the browser window edges on larger screen displays.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
     <div className="flex flex-col h-screen w-screen max-w-full overflow-hidden">
       <ClientNavbar />
       <main className="flex-grow flex flex-col justify-between overflow-hidden overflow-y-scroll bg-muted relative z-10">
-        <div className="w-full lg:w-11/12 mx-auto grow">{children}</div>
+        <div className="w-full lg:w-11/12 mx-auto grow my-6">{children}</div>
         <Footer showAdminIcon />
       </main>
     </div>
