11ff24c2979fe8c6a296ce4e25c11b3c8edfd7c4
xyashino <szczypiropan123@gmial.com>
2023-08-31 20:24:12 +0200

    Add ArticleWrapper component for rendering articles
    This commit introduces a new component: ArticleWrapper. This component is responsible for rendering markdown (MDX) content, and it also includes ImageTextSection for images, if available. It's part of an effort to modularise the article rendering process and make it cleaner and easier to understand.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

---
+++ src/layouts/ArticleWrapper.tsx
@@ -0,0 +1,20 @@
+import { MDXRemote } from "next-mdx-remote/rsc";
+import { ImageTextSection } from "@/components/ImageTextSection";
+
+type Props = BaseArticleMetadata & {
+  content: string;
+};
+export const ArticleWrapper = ({ content, ...rest }: Props) => {
+  return (
+    <div>
+      {"bgcSrc" in rest && rest.bgcSrc && rest.bgcAlt && (
+        <ImageTextSection src={rest.bgcSrc} alt={rest.bgcAlt} {...rest} />
+      )}
+      <div className="w-full bg-white p-12">
+        <article className="prose lg:mx-auto mx-2 max-w-none lg:w-5/6 w-full ">
+          <MDXRemote source={content} />
+        </article>
+      </div>
+    </div>
+  );
+};
