2b4d4714efa6ce05e11ab24c3a0296d7cefd9545
xyashino <szczypiropan123@gmial.com>
2023-08-31 20:19:00 +0200

    Update layout.tsx for better page presentation
    Removed unnecessary whitespace in main component styling for a cleaner look. The component width on large screens was also increased to "lg:w-11/12" for optimizing the viewing experience and enabling content growth.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -6,8 +6,8 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex flex-col h-screen w-screen max-w-full overflow-hidden">
       <ClientNavbar />
-      <main className="flex-grow flex flex-col justify-between overflow-hidden overflow-y-scroll   bg-muted relative z-10">
-        <div className="w-full lg:w-10/12 mx-auto">{children}</div>
+      <main className="flex-grow flex flex-col justify-between overflow-hidden overflow-y-scroll bg-muted relative z-10">
+        <div className="w-full lg:w-11/12 mx-auto grow">{children}</div>
         <Footer showAdminIcon />
       </main>
     </div>
