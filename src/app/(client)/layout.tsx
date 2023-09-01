555552137d9719e69d7863bd685b165b7eb7b15c
xyashino <szczypiropan123@gmial.com>
2023-09-01 10:10:50 +0200

    Update layout styling in layout.tsx
    Modified the CSS styling in layout.tsx to improve the UX design. The width of the main content div has been reduced slightly on large screens (from 11/12 to 10/12). Additional styling (rounded-xl) added to create rounded corners for a softer visual appearance. This change enhances the content readability and appearance for users.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -6,8 +6,10 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex flex-col h-screen w-screen max-w-full overflow-hidden">
       <ClientNavbar />
-      <main className="flex-grow flex flex-col justify-between overflow-hidden overflow-y-scroll bg-muted relative z-10">
-        <div className="w-full lg:w-11/12 mx-auto grow my-6">{children}</div>
+      <main className="flex-grow flex flex-col justify-between overflow-y-scroll bg-muted relative z-10">
+        <div className="w-full lg:w-10/12 mx-auto grow my-6 rounded-xl">
+          {children}
+        </div>
         <Footer showAdminIcon />
       </main>
     </div>
