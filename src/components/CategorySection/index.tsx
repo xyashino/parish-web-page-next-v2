d5aa772088d6a5bfba9b5bd21826062755c05612
xyashino <szczypiropan123@gmial.com>
2023-09-01 10:33:03 +0200

    Add CategorySection component
    This commit introduces a new CategorySection component. It is used to display a specified category's name and its associated albums, in a scrollable section. If there are no albums, an EmptySection will be shown. This component will facilitate a better organization and navigation of albums.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

---
+++ src/components/CategorySection/index.tsx
@@ -0,0 +1,32 @@
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { AlbumCard } from "@/components/CategorySection/AlbumCard";
+import { Separator } from "@/components/ui/separator";
+import { EmptySection } from "@/components/CategorySection/emptySection";
+import { RelationCategoriesResponse } from "@/types/category";
+
+export const CategorySection = ({
+  name,
+  albums,
+}: RelationCategoriesResponse) => {
+  return (
+    <section className="mb-12">
+      <div className="space-y-1 text-center">
+        <h2 className="text-2xl font-semibold tracking-tight text-center">
+          &ldquo; {name} &rdquo;
+        </h2>
+      </div>
+      <Separator className="my-4" />
+      <div className="relative">
+        <ScrollArea>
+          <div className="flex space-x-8 bg-muted p-4 rounded-xl">
+            {albums.map((album) => (
+              <AlbumCard key={album.id} {...album} />
+            ))}
+            {albums.length === 0 && <EmptySection />}
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+      </div>
+    </section>
+  );
+};
