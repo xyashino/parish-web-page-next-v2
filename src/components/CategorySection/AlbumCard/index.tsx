c4d1423809beb5cd1055517ea03ab4cd09174ba5
xyashino <szczypiropan123@gmial.com>
2023-09-01 10:32:22 +0200

    Add AlbumCard component to CategorySection
    This new feature includes the AlbumCard component under the CategorySection. This was added to enhance the user functionality by providing an album cover image and offering navigation options to individual album pages. Specific props were passed to the AlbumCard for
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

---
+++ src/components/CategorySection/AlbumCard/index.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+import { AlbumCoverImage } from "./albumCoverImage";
+import { useRouter } from "next/navigation";
+import { CategoriesAlbum } from "@/types/category";
+import { Navigation } from "@/types/enums";
+
+export const AlbumCard = ({ id, title, subtitle, cover }: CategoriesAlbum) => {
+  const { push } = useRouter();
+
+  return (
+    <div
+      className="space-y-3 cursor-pointer p-4 bg-background rounded-xl group w-[300px] lg:w-[400px] shadow"
+      onClick={() => push(`${Navigation.CLIENT_ALBUM}/${id}`)}
+    >
+      <div className="overflow-hidden rounded-md relative w-full aspect-video ">
+        <AlbumCoverImage cover={cover} title={title} />
+      </div>
+      <div className="space-y-1 text-sm">
+        <h3 className="font-lg font-bold">{title}</h3>
+        <p className="text-md text-muted-foreground">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
