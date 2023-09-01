246d275744586220b3f656730f5be1f5e92246fa
xyashino <szczypiropan123@gmial.com>
2023-09-01 10:32:41 +0200

    Add AlbumCoverImage component
    This new component, AlbumCoverImage, has been added to display the cover of albums in the Category section. If no cover is provided, a fallback ImageIcon is shown. This enhances the user experience by providing visual aid to users.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

---
+++ src/components/CategorySection/AlbumCard/albumCoverImage.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "next/image";
+import { ImageIcon } from "@radix-ui/react-icons";
+import { CategoriesCover } from "@/types/category";
+
+interface Props {
+  cover: CategoriesCover;
+  title: string;
+}
+
+export const AlbumCoverImage = ({ cover, title }: Props) => {
+  if (!cover || !cover.path) {
+    return (
+      <div className="w-full h-full grid place-items-center">
+        <ImageIcon className="w-full h-32 text-muted-foreground group-hover:scale-105 transition-transform" />
+      </div>
+    );
+  }
+
+  return (
+    <Image
+      src={cover.path}
+      alt={title ?? "Image display cover of album"}
+      className="object-center transition-transform group-hover:scale-105 absolute"
+      fill
+      draggable={false}
+    />
+  );
+};
