71f03ed3c8e2c4ed76622f155376f73dea6ef3e6
xyashino <szczypiropan123@gmial.com>
2023-09-04 15:44:56 +0200

    Add update function to weekIntentions in db
    This commit includes an update-week-intention function to the weekIntentions library and reorders the export sequence based on their functionalities. The new function allows for updating existing week intentions and enhances the overall efficiency and functionality of the weekIntentions component in db.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

--- src/lib/db/weekIntentions/index.ts
+++ src/lib/db/weekIntentions/index.ts
@@ -1,5 +1,6 @@
 export * from "./create-week-intentions";
 export * from "./delete-week-intentions";
 export * from "./get-many-week-intentions";
-export * from "./get-week-intentions-with-relations";
 export * from "./get-active-week-intentions";
+export * from "./update-week-intentions";
+export * from "./get-week-intentions-with-relations";
