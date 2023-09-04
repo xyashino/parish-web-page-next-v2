23887999c439baa5a0a8034637f83b660e5b7a41
xyashino <szczypiropan123@gmial.com>
2023-09-04 23:26:28 +0200

    Refactor common utilities to use 'WeekIntentionsDay'
    The module common.ts was adjusted to use the WeekIntentionsDay type instead of the previous Day type from "@prisma/client". Also, an unnecessary 'Day' import was removed. This change was necessary to fit the new structure of weekly schedulings, allowing us to interact with the DB entries in a more coherent manner.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

--- src/lib/utils/common.ts
+++ src/lib/utils/common.ts
@@ -1,8 +1,9 @@
-import { Day, Status, Weekday } from "@prisma/client";
+import { Status, Weekday } from "@prisma/client";
+import { WeekIntentionsDay } from "@/types/db/week-intentions";
 
 export const getLengthByEnumStatus = <T extends { status: Status }>(
   values: T[],
-  status: Status
+  status: Status,
 ) => values.filter((values) => values.status === status).length;
 
 export const getLengthByShow = <T extends { show: boolean }>(values: T[]) => {
@@ -17,7 +18,7 @@ export const getErrorMessage = (error: unknown) => {
   return errorMessage;
 };
 
-export const getActiveDayID = (days: Day[]) => {
+export const getActiveDayID = (days: WeekIntentionsDay[]) => {
   const getCurrentDay = new Date().getDay() - 1;
   const activeDay = Object.values(Weekday).at(getCurrentDay);
   return days.find((day) => day.day === activeDay)?.id ?? days[0].id;
