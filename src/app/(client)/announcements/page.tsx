03e6920f54e5f8e5911f08c8c75bdb838703e26b
xyashino <szczypiropan123@gmial.com>
2023-09-04 23:57:43 +0200

    Refactor fetching method for active announcements
    This commit replaces the older method for fetching active announcements with a newer, more efficient function. The function 'getActiveAnnouncement' was removed and replaced with an 'apiCall' function to fetch Announcement details. This is in line with our goal of enhancing our code by making API calls more standardized and flexible. Also, added a handling case for situations where no announcement is available.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

--- src/app/(client)/announcements/page.tsx
+++ src/app/(client)/announcements/page.tsx
@@ -1,12 +1,16 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { getActiveAnnouncement } from "@/lib/db/announcement";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { ImageTextSection } from "@/components/ImageTextSection";
+import { ApiRoute, RevalidateTag } from "@/types/enums";
+import { apiCall } from "@/lib/utils";
+import { AnnouncementResponse } from "@/types/db/announcement";
+import React from "react";
 
 export default async function Announcement() {
-  const data = await getActiveAnnouncement();
-  if (!data) return <div>null</div>;
-  const { subtitle, value } = data;
+  const [announcements] = await apiCall<AnnouncementResponse[]>(
+    ApiRoute.ACTIVE_ANNOUNCEMENT,
+    { next: { tags: [RevalidateTag.ANNOUNCEMENTS] } },
+  );
 
   return (
     <div className="my-4">
@@ -14,13 +18,19 @@ export default async function Announcement() {
         src="/biblia.webp"
         alt="Some hands"
         title="Ogłoszenia Parafialne"
-        subtitle={subtitle}
+        subtitle={announcements?.subtitle}
       />
       <Card className="mx-auto lg:w-11/12">
         <CardContent className="py-8 w-full lg:w-5/6 mx-auto">
-          <div className="mx-auto prose max-w-none">
-            <MDXRemote source={value} />
-          </div>
+          {announcements ? (
+            <div className="mx-auto prose max-w-none">
+              <MDXRemote source={announcements.value} />
+            </div>
+          ) : (
+            <h3 className="text-3xl text-foreground text-center uppercase italic font-light">
+              Spróbuj ponownie później
+            </h3>
+          )}
         </CardContent>
       </Card>
     </div>
