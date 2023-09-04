e97dc3c4c314ac3aefbece1023b4d13b51226433
xyashino <szczypiropan123@gmial.com>
2023-09-04 23:28:28 +0200

    Refactor LogoutAlert component for readability
    Removed hardcoded component 'triggerValue' and replaced it with in-line 'Button' component. This refactor simplifies code by directly inserting the 'Button' component, making it more readable and reusable. Also renamed props for consistency and coherence.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

--- src/components/alerts/LogoutAlert.tsx
+++ src/components/alerts/LogoutAlert.tsx
@@ -5,31 +5,27 @@ import { ExitIcon } from "@radix-ui/react-icons";
 import { Separator } from "@/components/ui/separator";
 import { ConfirmAlert } from "./ConfirmAlert";
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 interface Props {
   className?: string;
 }
 
-const triggerValue = (
-  <>
-    <ExitIcon className="text-background" />
-    <p className="text-background">Wyloguj</p>
-  </>
-);
 export const LogoutAlert = ({ className }: Props) => (
   <div className={cn("space-y-2 w-11/12 mx-auto", className)}>
     <Separator className="w-5/6 mx-auto" />
     <ConfirmAlert
-      headerData={{
+      headerConfig={{
         title: "Czy na pewno chcesz się wylogować?",
         description: "Zostaniesz przekierowany na stronę główną.",
       }}
-      triggerData={{
-        triggerValue,
-        className:
-          "w-full mx-auto flex items-center justify-center space-x-2.5 rounded-xl p-2.5",
-      }}
-      footerData={{
+      triggerItem={
+        <Button className="w-full text-background space-x-4 uppercase font-mono font-extrabold">
+          <ExitIcon />
+          <p>Wyloguj</p>
+        </Button>
+      }
+      footerConfig={{
         doAfterConfirm: () => signOut(),
       }}
     />
