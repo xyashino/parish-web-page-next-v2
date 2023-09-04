c68dc44eb3e0fb401ac25af0ef920ff29fa2675a
xyashino <szczypiropan123@gmial.com>
2023-09-04 23:23:21 +0200

    Add utility to convert API response to store data
    Introduced a new utility function 'convertIntentionsResponseToStoreData' in 'src/lib/utils'. This function translates the API response related to 'WeekIntentions' into a format suitable for updating the application store. By creating this function, data from the back-end can be manipulated into a structure that optimises rendering in the frontend, improving UX.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

---
+++ src/lib/utils/conver-intentions-response-to-store-data.tsx
@@ -0,0 +1,48 @@
+import {
+  DayIntentions,
+  WeekIntentionsStoreData,
+} from "@/types/interfaces/week-intentions-store.interface";
+import {
+  WeekIntentionsDay,
+  WeekIntentionsWithRelationsResponse,
+} from "@/types/db/week-intentions";
+import { Status } from "@prisma/client";
+
+type NotNullableResponse = Exclude<WeekIntentionsWithRelationsResponse, null>;
+
+const createDayIntentionMapEntry = ({
+  id,
+  dateOfDay,
+  ...rest
+}: WeekIntentionsDay): [string, DayIntentions] => [
+  id,
+  {
+    dateOfDay: dateOfDay ? new Date(dateOfDay) : null,
+    id,
+    ...rest,
+  },
+];
+
+export const convertIntentionsResponseToStoreData = ({
+  status = Status.NONE,
+  endWeek,
+  startWeek,
+  days,
+  id,
+}: NotNullableResponse): WeekIntentionsStoreData => {
+  const weekIntentions = {
+    status,
+    startWeek: startWeek ? new Date(startWeek) : null,
+    endWeek: endWeek ? new Date(endWeek) : null,
+    id,
+  };
+
+  const dayIntentionsMapData = days.map(createDayIntentionMapEntry);
+  const dayIntentions = new Map(dayIntentionsMapData);
+
+  return {
+    weekIntentions,
+    activeDay: days[0]?.id || "0",
+    dayIntentions,
+  };
+};
