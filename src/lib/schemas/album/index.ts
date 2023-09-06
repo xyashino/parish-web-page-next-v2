018a16a4c1b8655882df27d5b33c59ba77c9b1b3
xyashino <szczypiropan123@gmial.com>
2023-09-06 12:53:53 +0200

    Update validation messages in schemas
    The validation messages for empty fields in the 'categories' and 'album' schemas have been updated. They are now more descriptive, informing users that the 'name' in the category schema and 'title' in the album schema are required fields. This change aims to enhance user experience by giving them clear error messages.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

--- src/lib/schemas/album/index.ts
+++ src/lib/schemas/album/index.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const modifyAlbumSchema = z.object({
-  title: z.string().nonempty(),
+  title: z.string().nonempty({ message: "Tytuł jest wymagany." }),
   subtitle: z.string().optional(),
   categoryId: z.string().optional(),
   show: z.boolean().default(true),
--- src/lib/schemas/categories/index.ts
+++ src/lib/schemas/categories/index.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const modifyCategorySchema = z.object({
-  name: z.string().nonempty({ message: "Nazwa Kategorii nie może być pusta" }),
+  name: z.string().nonempty({ message: "Nazwa Kategorii jest wymagana." }),
   order: z.number().min(-20).max(20).default(0),
   show: z.boolean().default(true),
 });
