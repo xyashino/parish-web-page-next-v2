85b53e8ec3be36d77969f585fc6cf5dee770527c
xyashino <szczypiropan123@gmial.com>
2023-09-12 18:38:58 +0200

    Add Separator to layout and adjust margins
    The Separator component has been added to the RootLayout to clearly delineate different sections of the layout. Additionally, vertical margins (my-6) were only applied to larger screens to maintain the layout's responsiveness.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
     <div className="flex flex-col h-screen w-screen max-w-full overflow-hidden">
       <ClientNavbar />
       <main className="flex-grow flex flex-col justify-between overflow-y-scroll bg-muted relative ">
-        <div className="w-full lg:w-10/12 mx-auto grow my-6 rounded-xl">
+        <div className="w-full lg:w-10/12 mx-auto grow lg:my-6 rounded-xl">
           {children}
         </div>
         <Footer showAdminIcon />
