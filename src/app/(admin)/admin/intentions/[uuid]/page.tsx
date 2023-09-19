ea25042d150d2ed5bc290e4d8a023e5729fbf76b
xyashino <szczypiropan123@gmial.com>
2023-09-19 12:52:10 +0200

    Refactor type in function parameter
    Changed the type of uuid in the EditOneIntention function from 'any' to 'ParamsWithUUID'. This was done to provide better and strict type checking and thus reduce the likelihood of type-related bugs.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

--- src/app/(admin)/admin/intentions/[uuid]/page.tsx
+++ src/app/(admin)/admin/intentions/[uuid]/page.tsx
@@ -21,7 +21,7 @@ export async function generateStaticParams() {
   }));
 }
 
-const EditOneIntention = async ({ params: { uuid } }: any) => {
+const EditOneIntention = async ({ params: { uuid } }: ParamsWithUUID) => {
   const weekIntention = await apiCall<WeekIntentionsWithRelationsResponse>(
     `${ApiRoute.BASE_WEEK_INTENTIONS}/${uuid}`,
     {
