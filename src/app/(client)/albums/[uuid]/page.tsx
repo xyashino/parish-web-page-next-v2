43605e0ee290bf4213406308420342c7b45f21bd
xyashino <szczypiropan123@gmial.com>
2023-09-25 15:53:05 +0200

    Update import statements and refactor API call in albums page
    Modified the import statements in the albums page to match the latest updates in the types of the db in `@/types`. Removed the unnecessary import of `notFound` from `next/navigation`.
    
    Refactored the API call in `generateStaticParams` function to use `AlbumListResponse` instead of `AlbumsResponse`. Also code is updated to make sure that `images` array is treated as `ImageListResponse` for mapping operation. Redundant condition for returning `notFound` is also removed. These changes are made keeping in line with the recent updates in types in `@/types/db/album`.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

--- src/app/(client)/albums/[uuid]/page.tsx
+++ src/app/(client)/albums/[uuid]/page.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { notFound } from "next/navigation";
 import { ApiRoute, RevalidateTag } from "@/types/enums";
 import { apiCall } from "@/lib/utils";
-import { AlbumsResponse, AlbumWithRelationsResponse } from "@/types/db/album";
+import {
+  AlbumListResponse,
+  AlbumWithRelationsResponse,
+  ImageListResponse,
+} from "@/types/db/album";
 import { PageTitleWithPrevBtn } from "@/components/PageTitleWithPrevBtn";
 import { AlbumImage } from "@/components/album/AlbumImage";
 
 export async function generateStaticParams() {
-  const announcements = await apiCall<AlbumsResponse>(ApiRoute.BASE_ALBUMS, {
+  const announcements = await apiCall<AlbumListResponse>(ApiRoute.BASE_ALBUMS, {
     next: { tags: [RevalidateTag.ALBUMS] },
   });
   return announcements.map((announcement) => ({
@@ -23,14 +26,12 @@ const AlbumPage = async ({ params: { uuid } }: ParamsWithUUID) => {
     },
   );
 
-  if (!album) return notFound();
-
   const { images, title } = album;
   return (
     <div className="bg-white animate-fadeIn w-full h-full rounded shadow">
       <PageTitleWithPrevBtn title={`Album "${title}"`} />
       <div className="flex flex-wrap gap-8 w-full p-4 mx-auto justify-evenly">
-        {images.map((image, i) => (
+        {(images as ImageListResponse).map((image, i) => (
           <AlbumImage {...image} index={i} key={image.id} />
         ))}
       </div>
