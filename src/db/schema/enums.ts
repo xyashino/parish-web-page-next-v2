406e67c9db4016bc7d92b91c4ef3c966c37a5cef
xyashino <szczypiropan123@gmial.com>
2023-09-25 14:51:53 +0200

    Add new enums to schema file
    A new file, enums.ts, was added to the db schema. This file now contains definitions for "status" and "day" enums. This addition was necessary to provide enumerated values for these fields in the database for consistent data entry and retrieval.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

---
+++ src/db/schema/enums.ts
@@ -0,0 +1,11 @@
+import { pgEnum } from "drizzle-orm/pg-core";
+
+import {
+  DAY,
+  DAY_ENUM_NAME,
+  STATUS,
+  STATUS_ENUM_NAME,
+} from "../../lib/constants/db";
+
+export const statusEnum = pgEnum(STATUS_ENUM_NAME, STATUS);
+export const dayEnum = pgEnum(DAY_ENUM_NAME, DAY);
