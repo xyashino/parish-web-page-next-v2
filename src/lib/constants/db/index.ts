a9ab6c4e80e148c2f836f86e773a646a3aa08912
xyashino <szczypiropan123@gmial.com>
2023-09-25 14:53:45 +0200

    Add constants for database enums
    This commit introduces a new file that contains two enums, `STATUS` and `DAY`, for use within the database. It aims to provide uniformity in data entry, promote code reuse, and simplify the retrieval and manipulation of data. The `STATUS` enum will represent different states of an entity while `DAY` enum will be used to represent the week's days.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

---
+++ src/lib/constants/db/index.ts
@@ -0,0 +1,13 @@
+export const STATUS_ENUM_NAME = "status" as const;
+export const STATUS = ["ACTIVE", "UPCOMING", "NONE"] as const;
+
+export const DAY_ENUM_NAME = "day" as const;
+export const DAY = [
+  "MONDAY",
+  "TUESDAY",
+  "WEDNESDAY",
+  "THURSDAY",
+  "FRIDAY",
+  "SATURDAY",
+  "SUNDAY",
+] as const;
