779792ff0c60d7cc0550684030ae054e7e8ca9dd
xyashino <szczypiropan123@gmial.com>
2023-09-25 15:07:11 +0200

    Add DefaultCrud class for database operations
    A new file 'default.ts' has been added to src/db/handlers directory. This file includes the new DefaultCrud class to enable CRUD operations in the database. This class empowers us with create, update, delete, and find (one or all) operations on a given PostgreSQL table model. It uses drizzle-orm for managing these operations and also introduces error handling for these operations. This addition would reduce the boilerplate code required for basic database operations and provide consistency across all DB operations.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

---
+++ src/db/handlers/default.ts
@@ -0,0 +1,97 @@
+import { db } from "@/db/client";
+import { createInsertSchema } from "drizzle-zod";
+import { eq, InferInsertModel } from "drizzle-orm";
+import { uuidSchema } from "@/lib/schemas/default";
+import { PgTable } from "drizzle-orm/pg-core";
+
+export class DefaultCrud<T extends PgTable> {
+  protected db = db;
+
+  constructor(
+    protected model: T,
+    protected insertSchema: ReturnType<
+      typeof createInsertSchema
+    > = createInsertSchema(model),
+  ) {}
+
+  protected async withErrorHandling<R, D = null>(
+    fn: () => Promise<R>,
+    defaultValue: D = null as unknown as D,
+  ): Promise<R | D> {
+    try {
+      return await fn();
+    } catch (error) {
+      console.error(error);
+      return defaultValue;
+    }
+  }
+
+  async create(data: InferInsertModel<T>): Promise<T["$inferSelect"] | null> {
+    const { withErrorHandling, db, model, insertSchema } = this;
+    return withErrorHandling(async () => {
+      const parsedData = insertSchema.parse(data);
+      const [result] = (await db
+        .insert(model)
+        .values(parsedData)
+        .returning()) as unknown[];
+      return result as T["$inferSelect"];
+    });
+  }
+
+  async update<Type>(
+    id: string,
+    data: InferInsertModel<T>,
+  ): Promise<T["$inferSelect"] | null> {
+    const { withErrorHandling, db, model, insertSchema } = this;
+    return withErrorHandling(async () => {
+      const { uuid } = uuidSchema.parse({ uuid: id });
+      if (!("id" in model)) throw new Error("Model does not have an id column");
+      const parsedData = insertSchema.parse(data);
+      const [result] = (await db
+        .update(model)
+        .set(parsedData)
+        .where(eq(model.id as any, uuid))
+        .returning()) as unknown[];
+      return result as T["$inferSelect"];
+    });
+  }
+
+  async delete(id: string): Promise<T["$inferSelect"] | null> {
+    const { withErrorHandling, db, model } = this;
+
+    return withErrorHandling(async () => {
+      if (!("id" in model)) throw new Error("Model does not have an id column");
+      const { uuid } = uuidSchema.parse({ uuid: id });
+      const [result] = (await db
+        .delete(model)
+        .where(eq(model.id as any, uuid))
+        .returning()) as unknown[];
+      return result as T["$inferSelect"];
+    });
+  }
+
+  async findOne(id: string): Promise<T["$inferSelect"] | null> {
+    const { withErrorHandling, db, model } = this;
+    return withErrorHandling(async () => {
+      if (!("id" in model)) throw new Error("Model does not have an id column");
+
+      const { uuid } = uuidSchema.parse({ uuid: id });
+      console.log({ uuid });
+      const [result] = (await db
+        .select()
+        .from(model)
+        .where(eq(model.id as any, uuid))) as unknown[];
+
+      return result as T["$inferSelect"];
+    });
+  }
+
+  async findAll<Type>(): Promise<T["$inferSelect"][]> {
+    const { withErrorHandling, db, model } = this;
+
+    return withErrorHandling(async () => {
+      const result = (await db.select().from(model)) as unknown[];
+      return result as T["$inferSelect"][];
+    }, []);
+  }
+}
