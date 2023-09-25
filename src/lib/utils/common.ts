d0ed0b800cf27a5b03fb09dc4cb29158ecd81f8b
xyashino <szczypiropan123@gmial.com>
2023-09-25 15:01:50 +0200

    Update necessary imports for improved type compatibility
    Changes have been made to the 'utils/common.ts'. We no longer import 'Status' and 'Weekday' from '@prisma/client'. Instead, 'DAY' has been imported from '@/lib/constants/db'. The function 'getLengthByEnumStatus' now accepts any 'status' type. This change is intended to improve type compatibility and flexibility, preventing possible issues in future updates or extensions to the status types.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

--- src/lib/utils/common.ts
+++ src/lib/utils/common.ts
@@ -1,9 +1,9 @@
-import { Status, Weekday } from "@prisma/client";
 import { WeekIntentionsDay } from "@/types/db/week-intentions";
+import { DAY } from "@/lib/constants/db";
 
-export const getLengthByEnumStatus = <T extends { status: Status }>(
+export const getLengthByEnumStatus = <T extends { status: any }>(
   values: T[],
-  status: Status,
+  status: any,
 ) => values.filter((values) => values.status === status).length;
 
 export const getLengthByShow = <T extends { show: boolean }>(values: T[]) => {
@@ -20,6 +20,6 @@ export const getErrorMessage = (error: unknown) => {
 
 export const getActiveDayID = (days: WeekIntentionsDay[]) => {
   const getCurrentDay = new Date().getDay() - 1;
-  const activeDay = Object.values(Weekday).at(getCurrentDay);
+  const activeDay = DAY.at(getCurrentDay);
   return days.find((day) => day.day === activeDay)?.id ?? days[0].id;
 };
