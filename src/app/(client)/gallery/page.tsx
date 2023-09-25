52d4ef0e1f5b80b79c6be4fa3a4fb72538131e9e
xyashino <szczypiropan123@gmial.com>
2023-09-25 16:05:40 +0200

    Refactor Gallery page to use apiCall function
    Refactored the Gallery page to use the generic apiCall function for getting active categories instead of the specific 'getCategoriesWithAlbums' function. This change enables adding revalidation tags for categories, albums and images which improves app performance by reducing unnecessary revalidations.
    
    Signed-off-by: xyashino <szczypiropan123@gmial.com>

--- src/app/(client)/gallery/page.tsx
+++ src/app/(client)/gallery/page.tsx
@@ -1,10 +1,22 @@
 import { redirect } from "next/navigation";
-import { getCategoriesWithAlbums } from "@/lib/db/category";
 import { CategorySection } from "@/components/CategorySection";
-import { Navigation } from "@/types/enums";
+import { ApiRoute, Navigation, RevalidateTag } from "@/types/enums";
+import { apiCall } from "@/lib/utils";
+import { ActiveCategoriesResponse } from "@/types/db/album";
 
 export default async function Gallery() {
-  const categories = await getCategoriesWithAlbums();
+  const categories = await apiCall<ActiveCategoriesResponse>(
+    ApiRoute.ACTIVE_CATEGORIES,
+    {
+      next: {
+        tags: [
+          RevalidateTag.CATEGORIES,
+          RevalidateTag.ALBUMS,
+          RevalidateTag.IMAGES,
+        ],
+      },
+    },
+  );
   if (categories.length === 0) redirect(Navigation.CLIENT_HOME);
   return (
     <div className="bg-background flex flex-col h-full my-auto shadowflex p-4 w-full animate-fadeIn">
